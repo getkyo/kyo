64710090d5488d1da5d366dcdd025593906a77a3
Flavio Brasil <fwbrasil@gmail.com>
2023-02-04 00:30:32 -0800

    disable opt inlining for now

--- opt-gen.sh
+++ opt-gen.sh
@@ -14,32 +14,32 @@ cp -r ./kyo-core/src/* ./kyo-core-opt3/src/
 
 find .
 
-echo "opt3 inlining"
-if [[ -z "$GITHUB_BASE_REF" ]]
-then
-    find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(1\)\\\*\\//inline/g {} +
-    find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(2\)\\\*\\//inline/g {} +
-    find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(3\)\\\*\\//inline/g {} +
-else
-    find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(1\)\*\//inline/g' "{}"
-    find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(2\)\*\//inline/g' "{}"
-    find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(3\)\*\//inline/g' "{}"
-fi
+# echo "opt3 inlining"
+# if [[ -z "$GITHUB_BASE_REF" ]]
+# then
+#     find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(1\)\\\*\\//inline/g {} +
+#     find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(2\)\\\*\\//inline/g {} +
+#     find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(3\)\\\*\\//inline/g {} +
+# else
+#     find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(1\)\*\//inline/g' "{}"
+#     find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(2\)\*\//inline/g' "{}"
+#     find ./kyo-core-opt3/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(3\)\*\//inline/g' "{}"
+# fi
 
-echo "opt2 inlining"
-if [[ -z "$GITHUB_BASE_REF" ]]
-then
-    find ./kyo-core-opt2/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(2\)\\\*\\//inline/g {} +
-    find ./kyo-core-opt2/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(3\)\\\*\\//inline/g {} +
-else
-    find ./kyo-core-opt2/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(2\)\*\//inline/g' {} \;
-    find ./kyo-core-opt2/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(3\)\*\//inline/g' {} \;
-fi
+# echo "opt2 inlining"
+# if [[ -z "$GITHUB_BASE_REF" ]]
+# then
+#     find ./kyo-core-opt2/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(2\)\\\*\\//inline/g {} +
+#     find ./kyo-core-opt2/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(3\)\\\*\\//inline/g {} +
+# else
+#     find ./kyo-core-opt2/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(2\)\*\//inline/g' {} \;
+#     find ./kyo-core-opt2/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(3\)\*\//inline/g' {} \;
+# fi
 
-echo "opt1 inlining"
-if [[ -z "$GITHUB_BASE_REF" ]]
-then
-    find ./kyo-core-opt1/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(3\)\\\*\\//inline/g {} +
-else
-    find ./kyo-core-opt1/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(3\)\*\//inline/g' {} \;
-fi
+# echo "opt1 inlining"
+# if [[ -z "$GITHUB_BASE_REF" ]]
+# then
+#     find ./kyo-core-opt1/src -type f -name '*.scala' -exec sed -i '' s/\\/\\\*inline\(3\)\\\*\\//inline/g {} +
+# else
+#     find ./kyo-core-opt1/src -type f -name '*.scala' -exec sed -i 's/\/\*inline\(3\)\*\//inline/g' {} \;
+# fi
