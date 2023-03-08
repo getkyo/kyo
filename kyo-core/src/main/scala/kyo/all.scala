026526b6ffb33f511298d29ebbc42174ffab30e7
Flavio Brasil <fwbrasil@gmail.com>
2023-03-07 23:42:57 -0800

    update kyo.all

--- kyo-core/src/main/scala/kyo/all.scala
+++ kyo-core/src/main/scala/kyo/all.scala
@@ -9,13 +9,19 @@ object all {
   export envs._
   export ios._
   export lists._
+  export locals._
+  export loggers._
   export options._
   export randoms._
   export resources._
   export tries._
   export concurrent.adders._
   export concurrent.atomics._
+  export concurrent.channels._
   export concurrent.fibers._
   export concurrent.latches._
+  export concurrent.meters._
+  export concurrent.queues._
   export concurrent.timers._
+  val KyoApp = kyo.KyoApp
 }
