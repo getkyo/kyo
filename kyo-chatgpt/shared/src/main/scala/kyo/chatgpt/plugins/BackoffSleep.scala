d80eeab3de14b9b92ca41d6e46e5dac338259fb9
Flavio Brasil <fwbrasil@gmail.com>
2023-10-12 12:16:14 -0700

    rename sleep plugin

--- kyo-chatgpt/shared/src/main/scala/kyo/chatgpt/plugins/BackoffSleep.scala
+++ kyo-chatgpt/shared/src/main/scala/kyo/chatgpt/plugins/BackoffSleep.scala
@@ -4,11 +4,11 @@ import kyo._
 import scala.concurrent.duration._
 import kyo.concurrent.fibers.Fibers
 
-object Sleep {
+object BackoffSleep {
   val plugin = Plugins.init[Int, Unit](
       "backoff_sleep_millis",
       "Sleeps for the specified number of milliseconds. Feel free to use this plugin as backoff " +
-        "mechanism but use a reasonable backoff policy to ensure you're able to fullfil the " +
+        "mechanism but use a reasonable backoff policy to ensure you're able to fullfill the " +
         "user's request in a reasonable time frame."
   ) { v =>
     Fibers.sleep(v.millis).unit
