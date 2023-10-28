4ab02c19dfb6aec523cc14ce45dad603073ef39e
Flavio Brasil <fwbrasil@gmail.com>
2023-10-28 12:05:27 -0300

    chatgpt: improve json schema descriptions

--- kyo-chatgpt/shared/src/main/scala/kyo/chatgpt/util/JsonSchema.scala
+++ kyo-chatgpt/shared/src/main/scala/kyo/chatgpt/util/JsonSchema.scala
@@ -25,59 +25,59 @@ object JsonSchema {
     c.collect {
       case desc(v) =>
         "description" -> Json.Str(v.stripMargin)
-    }.toList
+    }.distinct.toList
 
-  def convert(schema: Schema[_]): List[(String, Json)] =
-    desc(schema.annotations) ++ {
-      schema match {
-        case Schema.Primitive(StandardType.StringType, _) =>
-          List("type" -> Json.Str("string"))
+  def convert(schema: Schema[_]): List[(String, Json)] = {
+    def desc = this.desc(schema.annotations)
+    schema match {
+      case Schema.Primitive(StandardType.StringType, _) =>
+        desc ++ List("type" -> Json.Str("string"))
 
-        case Schema.Primitive(StandardType.IntType, _) =>
-          List("type" -> Json.Str("integer"), "format" -> Json.Str("int32"))
+      case Schema.Primitive(StandardType.IntType, _) =>
+        desc ++ List("type" -> Json.Str("integer"), "format" -> Json.Str("int32"))
 
-        case Schema.Primitive(StandardType.LongType, _) =>
-          List("type" -> Json.Str("integer"), "format" -> Json.Str("int64"))
+      case Schema.Primitive(StandardType.LongType, _) =>
+        desc ++ List("type" -> Json.Str("integer"), "format" -> Json.Str("int64"))
 
-        case Schema.Primitive(StandardType.DoubleType, _) =>
-          List("type" -> Json.Str("number"))
+      case Schema.Primitive(StandardType.DoubleType, _) =>
+        desc ++ List("type" -> Json.Str("number"))
 
-        case Schema.Primitive(StandardType.FloatType, _) =>
-          List("type" -> Json.Str("number"), "format" -> Json.Str("float"))
+      case Schema.Primitive(StandardType.FloatType, _) =>
+        desc ++ List("type" -> Json.Str("number"), "format" -> Json.Str("float"))
 
-        case Schema.Primitive(StandardType.BoolType, _) =>
-          List("type" -> Json.Str("boolean"))
+      case Schema.Primitive(StandardType.BoolType, _) =>
+        desc ++ List("type" -> Json.Str("boolean"))
 
-        case Schema.Optional(innerSchema, _) =>
-          convert(innerSchema)
+      case Schema.Optional(innerSchema, _) =>
+        convert(innerSchema)
 
-        case Schema.Sequence(innerSchema, _, _, _, _) =>
-          List("type" -> Json.Str("array"), "items" -> Json.Obj(convert(innerSchema).toSeq: _*))
+      case Schema.Sequence(innerSchema, _, _, _, _) =>
+        List("type" -> Json.Str("array"), "items" -> Json.Obj(convert(innerSchema): _*))
 
-        case schema: Schema.Enum[_] =>
-          val cases = schema.cases.map(c => Json.Obj(convert(c.schema).toSeq: _*))
-          List("oneOf" -> Json.Arr(cases: _*))
+      case schema: Schema.Enum[_] =>
+        val cases = schema.cases.map(c => Json.Obj(convert(c.schema): _*))
+        List("oneOf" -> Json.Arr(cases: _*))
 
-        case schema: Schema.Record[_] =>
-          val properties = schema.fields.foldLeft(List.empty[(String, Json)]) { (acc, field) =>
-            acc :+ (field.name -> Json.Obj(
-                (convert(field.schema) ++ desc(field.annotations)).toSeq: _*
-            ))
-          }
-          val requiredFields = schema.fields.collect {
-            case field if !field.schema.isInstanceOf[Schema.Optional[_]] => Json.Str(field.name)
-          }
-          List(
-              "type"       -> Json.Str("object"),
-              "properties" -> Json.Obj(properties.toSeq: _*),
-              "required"   -> Json.Arr(requiredFields: _*)
-          )
+      case schema: Schema.Record[_] =>
+        val properties = schema.fields.foldLeft(List.empty[(String, Json)]) { (acc, field) =>
+          acc :+ (field.name -> Json.Obj(
+              (this.desc(field.annotations) ++ convert(field.schema)): _*
+          ))
+        }
+        val requiredFields = schema.fields.collect {
+          case field if !field.schema.isInstanceOf[Schema.Optional[_]] => Json.Str(field.name)
+        }
+        desc ++ List(
+            "type"       -> Json.Str("object"),
+            "properties" -> Json.Obj(properties.toSeq: _*),
+            "required"   -> Json.Arr(requiredFields: _*)
+        )
 
-        case schema: Schema.Lazy[_] =>
-          convert(schema.schema)
+      case schema: Schema.Lazy[_] =>
+        convert(schema.schema)
 
-        case _ =>
-          throw new UnsupportedOperationException("This schema type is not supported")
-      }
+      case _ =>
+        throw new UnsupportedOperationException("This schema type is not supported")
     }
+  }
 }
