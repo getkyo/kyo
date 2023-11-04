c1e2752c5137190869a7b45876d5521196ce6e05
Flavio Brasil <fwbrasil@gmail.com>
2023-11-04 10:47:45 -0700

    improve error messages for kyo-direct in scala 3

--- kyo-direct/src/main/scala-3/kyo/Validate.scala
+++ kyo-direct/src/main/scala-3/kyo/Validate.scala
@@ -17,17 +17,23 @@ private[kyo] object Validate {
         case Apply(TypeApply(Ident("await"), List(t, s)), List(v)) => true
       }
 
+    def show(t: Tree) =
+      try t.show
+      catch {
+        case ex: java.lang.Throwable => ""
+      }
+
     Trees.traverse(tree) {
       case Apply(TypeApply(Ident("await"), List(t, s)), List(v)) => // Do nothing
       case Apply(TypeApply(Ident("defer"), List(t)), List(v)) =>
         fail(tree, "Nested `defer` blocks are not allowed.")
       case tree: Term if tree.tpe.typeSymbol.name == ">" =>
         fail(tree, "Effectful computation must be inside an `await` block.")
-      case tree @ ValDef(_, _, _) if tree.show.startsWith("var ") =>
+      case tree @ ValDef(_, _, _) if show(tree).startsWith("var ") =>
         fail(tree, "`var` declarations are not allowed inside a `defer` block.")
       case Return(_, _) =>
         fail(tree, "`return` statements are not allowed inside a `defer` block.")
-      case tree @ ValDef(_, _, _) if tree.show.startsWith("lazy val ") =>
+      case tree @ ValDef(_, _, _) if show(tree).startsWith("lazy val ") =>
         fail(tree, "`lazy val` declarations are not allowed inside a `defer` block.")
       case Lambda(_, body) if !pure(body) =>
         fail(tree, "Lambda functions containing `await` are not supported.")
