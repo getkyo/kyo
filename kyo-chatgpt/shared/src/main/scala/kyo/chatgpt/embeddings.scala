d3216ee518185948792e6b6d5f7112a1889282cb
Flavio Brasil <fwbrasil@gmail.com>
2023-11-09 22:39:43 -0800

    chatgpt: embeddings logging

--- kyo-chatgpt/shared/src/main/scala/kyo/chatgpt/embeddings.scala
+++ kyo-chatgpt/shared/src/main/scala/kyo/chatgpt/embeddings.scala
@@ -10,6 +10,7 @@ import kyo.requests._
 import sttp.client3._
 import sttp.client3.ziojson._
 import zio.json._
+import kyo.loggers._
 
 object embeddings {
 
@@ -18,17 +19,24 @@ object embeddings {
   object Embeddings {
     import internal._
 
-    def apply(text: String, model: String = "text-embedding-ada-002"): Embedding > AIs =
-      Configs.get.map { config =>
-        Requests[Response](
+    private val logger = Loggers.init(getClass)
+
+    def init(text: String, model: String = "text-embedding-ada-002"): Embedding > AIs =
+      for {
+        apiKey <- Configs.apiKey
+        config <- Configs.get
+        req = Request(text, model)
+        _ <- logger.debug(req.toJsonPretty)
+        res <- Requests[Response](
             _.contentType("application/json")
-              .header("Authorization", s"Bearer ${config.apiKey}")
+              .header("Authorization", s"Bearer $apiKey")
               .post(uri"${config.apiUrl}/v1/embeddings")
-              .body(Request(text, model))
+              .body(req)
               .response(asJson[Response])
-        ).map { value =>
-          Embedding(value.usage.prompt_tokens, value.data.head.embedding)
-        }
+        )
+        _ <- logger.debug(res.toJsonPretty)
+      } yield {
+        Embedding(res.usage.prompt_tokens, res.data.head.embedding)
       }
 
     private object internal {
@@ -38,6 +46,9 @@ object embeddings {
       case class Response(data: List[Data], usage: Usage)
 
       implicit val requestEncoder: JsonEncoder[Request]   = DeriveJsonEncoder.gen[Request]
+      implicit val dataEncoder: JsonEncoder[Data]         = DeriveJsonEncoder.gen[Data]
+      implicit val usageEncoder: JsonEncoder[Usage]       = DeriveJsonEncoder.gen[Usage]
+      implicit val responseEncoder: JsonEncoder[Response] = DeriveJsonEncoder.gen[Response]
       implicit val dataDecoder: JsonDecoder[Data]         = DeriveJsonDecoder.gen[Data]
       implicit val usageDecoder: JsonDecoder[Usage]       = DeriveJsonDecoder.gen[Usage]
       implicit val responseDecoder: JsonDecoder[Response] = DeriveJsonDecoder.gen[Response]
