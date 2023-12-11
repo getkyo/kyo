ee3b8129a2c94a4b16e2545b11852adbaaf52099
Flavio Brasil <fwbrasil@gmail.com>
2023-12-11 10:49:01 -0800

    requests: more convenient API to customize the underlying HttpClient

--- kyo-sttp/jvm/src/main/scala/kyo/PlatformBackend.scala
+++ kyo-sttp/jvm/src/main/scala/kyo/PlatformBackend.scala
@@ -2,8 +2,21 @@ package kyo
 
 import kyo.requests.Backend
 import sttp.client3._
+import java.net.http.HttpClient
+import kyo.internal.KyoSttpMonad
+import sttp.capabilities.WebSockets
 
 private[kyo] object PlatformBackend {
+
+  def apply(backend: SttpBackend[KyoSttpMonad.M, WebSockets]): Backend =
+    new Backend {
+      def send[T](r: Request[T, Any]) =
+        r.send(backend)
+    }
+
+  def apply(client: HttpClient): Backend =
+    apply(HttpClientKyoBackend.usingClient(client))
+
   val default =
     new Backend {
       val b = HttpClientKyoBackend()
