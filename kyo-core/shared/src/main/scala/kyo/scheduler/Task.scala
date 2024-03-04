d28857bc94587ece69edaed5cc8f24cd7053df2a
Flavio Brasil <fwbrasil@gmail.com>
2024-03-04 08:02:47 -0800

    reorganize scheduler package

--- kyo-core/shared/src/main/scala/kyo/scheduler/Task.scala
+++ kyo-core/shared/src/main/scala/kyo/scheduler/Task.scala
@@ -1,5 +1,8 @@
 package kyo.scheduler
 
+import kyo.Logs
+import scala.util.control.NonFatal
+
 trait Task extends Ordered[Task]:
     def compare(that: Task) =
         (that.runtime() - runtime()).asInstanceOf[Int]
@@ -12,4 +15,17 @@ object Task:
     opaque type Result = Boolean
     val Preempted: Result = true
     val Done: Result      = false
+
+    inline def apply(inline r: => Unit): Task =
+        new Task:
+            def runtime() = 0
+            def preempt() = {}
+            def run() =
+                try r
+                catch
+                    case ex if NonFatal(ex) =>
+                        Logs.logger.error("Failed task.")
+                end try
+                Task.Done
+            end run
 end Task
