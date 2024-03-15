4cd231c26eeb5953775ec5fcd0b4093e9dc8c6db
Flavio Brasil <fwbrasil@gmail.com>
2024-03-15 15:11:58 -0700

    minor optimization

--- kyo-core/shared/src/main/scala/kyo/envs.scala
+++ kyo-core/shared/src/main/scala/kyo/envs.scala
@@ -24,25 +24,23 @@ final class Envs[E] private[kyo] (using private val tag: Tag[E])
     def use[T, S](f: E => T < S): T < (Envs[E] & S) =
         get.map(f)
 
-    def run[T, S](e: E < S)(v: T < (Envs[E] & S))(using f: Flat[T < (Envs[E] & S)]): T < S =
-        e.map { e =>
-            given Handler[Env[E]#Value, Envs[E], Any] =
-                new Handler[Env[E]#Value, Envs[E], Any]:
-                    def pure[U: Flat](v: U) = v
-                    def apply[U, V: Flat, S2](
-                        m: Env[E]#Value[U],
-                        f: U => V < (Envs[E] & S2)
-                    ): V < (S2 & Envs[E]) =
-                        m match
-                            case Input =>
-                                f(e.asInstanceOf[U])
-                            case _ =>
-                                f(m.asInstanceOf[U])
-            this.handle[T, Envs[E] & S, Any](v).map {
-                case Input => e.asInstanceOf[T]
-                case r     => r
-            }.asInstanceOf[T < S]
-        }
+    def run[T, S](e: E)(v: T < (Envs[E] & S))(using f: Flat[T < (Envs[E] & S)]): T < S =
+        given Handler[Env[E]#Value, Envs[E], Any] =
+            new Handler[Env[E]#Value, Envs[E], Any]:
+                def pure[U: Flat](v: U) = v
+                def apply[U, V: Flat, S2](
+                    m: Env[E]#Value[U],
+                    f: U => V < (Envs[E] & S2)
+                ): V < (S2 & Envs[E]) =
+                    m match
+                        case Input =>
+                            f(e.asInstanceOf[U])
+                        case _ =>
+                            f(m.asInstanceOf[U])
+        this.handle[T, Envs[E] & S, Any](v).map {
+            case Input => e.asInstanceOf[T]
+            case r     => r
+        }.asInstanceOf[T < S]
     end run
 
     override def accepts[M2[_], E2 <: Effect[M2, E2]](other: Effect[M2, E2]) =
