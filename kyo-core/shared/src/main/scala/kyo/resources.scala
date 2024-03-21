e888dd7cbc3426bdfe93700ab7101872339a94cf
Flavio Brasil <fwbrasil@gmail.com>
2024-03-21 11:54:20 -0700

    minor Resources improvement

--- kyo-core/shared/src/main/scala/kyo/resources.scala
+++ kyo-core/shared/src/main/scala/kyo/resources.scala
@@ -6,14 +6,14 @@ opaque type Resources <: IOs = IOs
 
 object Resources:
 
-    private val local = Locals.init[Queues.Unbounded[() => Unit < IOs] | None.type](None)
+    private val local = Locals.init[Queues.Unbounded[Unit < IOs] | None.type](None)
 
     def ensure(v: => Unit < IOs): Unit < Resources =
         local.use {
             case None =>
                 bug("Can't locate Resources finalizer queue.")
-            case q: Queues.Unbounded[() => Unit < IOs] =>
-                q.offer(() => v).map {
+            case q: Queues.Unbounded[Unit < IOs] =>
+                q.offer(IOs(v)).map {
                     case true => ()
                     case false =>
                         bug("Resources finalizer queue already closed.")
@@ -25,13 +25,13 @@ object Resources:
         ensure(v.close()).andThen(v)
 
     def run[T, S](v: T < (Resources & S)): T < (IOs & S) =
-        Queues.initUnbounded[() => Unit < IOs](Access.Mpsc).map { q =>
+        Queues.initUnbounded[Unit < IOs](Access.Mpsc).map { q =>
             def close: Unit < IOs =
                 q.close.map {
                     case None =>
                         bug("Resources finalizer queue already closed.")
                     case Some(l) =>
-                        Seqs.collect(l.map(_())).unit
+                        Seqs.collectUnit(l)
                 }
             IOs.ensure(close) {
                 local.let(q)(v)
