60c204ab38119b31f33546da4d552adf58011e06
Flavio Brasil <fwbrasil@gmail.com>
2024-03-21 14:04:42 -0700

    refactor layers test

--- kyo-core/shared/src/test/scala/kyoTest/layersTest.scala
+++ kyo-core/shared/src/test/scala/kyoTest/layersTest.scala
@@ -3,12 +3,10 @@ package kyoTest
 import kyo.*
 import scala.util.Failure
 
-class layersTest extends KyoTest:
-
+class LayersTest extends KyoTest:
     case class Dep1(int: Int)
     case class Dep2(str: String)
     case class Dep3(bool: Boolean)
-
     case class Dep(dep1: Int, dep2: String, dep3: Boolean)
 
     val depLayer  = Envs[Dep].layer(Dep(1, "hello", true))
@@ -18,21 +16,19 @@ class layersTest extends KyoTest:
 
     "Envs layers should be composable and provide multiple dependencies" in {
         val layer = dep1Layer.andThen(dep2Layer).andThen(dep3Layer).chain(depLayer)
-
         val effect =
             for
                 dep1 <- Envs[Dep1].get
                 dep2 <- Envs[Dep2].get
                 dep3 <- Envs[Dep3].get
             yield s"${dep1.int}-${dep2.str}-${dep3.bool}"
-
         val handledEffect = layer.run(effect)
-
         assert(handledEffect == "1-hello-true")
     }
 
     case class TestError1(msg: String)
     case class TestError2(msg: String)
+
     val stringToTE1Layer = Aborts[String].layer(str => Aborts[TestError1].fail(TestError1(str)))
     val dep1ToTE1Layer   = Aborts[Dep2].layer(dep => Aborts[TestError1].fail(TestError1(dep.str)))
     val throwableToTE1Layer =
@@ -40,60 +36,44 @@ class layersTest extends KyoTest:
     val testError1ToTE2Layer =
         Aborts[TestError1].layer(err => Aborts[TestError2].fail(TestError2(err.msg)))
 
-    "Aborts layers should be composable and handle multiple error types" in {
-        val layer =
-            stringToTE1Layer.andThen(dep1ToTE1Layer).andThen(throwableToTE1Layer).chain(
-                testError1ToTE2Layer
-            )
+    "Aborts layers should handle string failures" in {
+        val layer  = stringToTE1Layer.chain(testError1ToTE2Layer)
+        val effect = Aborts[String].fail("string failure")
+        assert(Aborts[TestError2].run(layer.run(effect)) == Left(TestError2("string failure")))
+    }
 
-        val effect1 =
-            for
-                _ <- Aborts[String].fail("string failure")
-                _ <- Aborts[Dep2].fail(Dep2("dep2 failure"))
-                _ <- Aborts[Throwable].fail(new Exception("throwable failure"))
-            yield ()
+    "Aborts layers should handle Dep2 failures" in {
+        val layer  = dep1ToTE1Layer.chain(testError1ToTE2Layer)
+        val effect = Aborts[Dep2].fail(Dep2("dep2 failure"))
+        assert(Aborts[TestError2].run(layer.run(effect)) == Left(TestError2("dep2 failure")))
+    }
 
-        val effect2 =
-            for
-                _ <- Aborts[Throwable].fail(new Exception("throwable failure"))
-                _ <- Aborts[String].fail("string failure")
-                _ <- Aborts[Dep2].fail(Dep2("dep2 failure"))
-            yield ()
-
-        val effect3 =
-            for
-                _ <- Aborts[Dep2].fail(Dep2("dep2 failure"))
-                _ <- Aborts[Throwable].fail(new Exception("throwable failure"))
-                _ <- Aborts[String].fail("string failure")
-            yield ()
-
-        assert(Aborts[TestError2].run(layer.run(effect1)) == Left(TestError2("string failure")))
-        assert(Aborts[TestError2].run(layer.run(effect2)) == Left(TestError2("throwable failure")))
-        assert(Aborts[TestError2].run(layer.run(effect3)) == Left(TestError2("dep2 failure")))
+    "Aborts layers should handle Throwable failures" in {
+        val layer  = throwableToTE1Layer.chain(testError1ToTE2Layer)
+        val effect = Aborts[Throwable].fail(new Exception("throwable failure"))
+        assert(Aborts[TestError2].run(layer.run(effect)) == Left(TestError2("throwable failure")))
     }
 
     val optionsToAbortsLayer = Options.layer(Aborts[String].fail("missing value"))
     val optionsToTriesLayer  = Options.layer(IOs.fail("missing value"))
 
-    "Options layer should handle None as other failures" in {
-        val effect =
-            for
-                _ <- Options.get(None)
-            yield ()
-
+    "Options layer should handle None as Aborts failure" in {
+        val effect                = Options.get(None)
         val effectHandledToAborts = optionsToAbortsLayer.run(effect)
-        val effectHandledToTries  = optionsToTriesLayer.run(effect)
-
         assert {
             Aborts[String].run(effectHandledToAborts) match
                 case Left("missing value") => true
                 case _                     => false
         }
+    }
 
+    "Options layer should handle None as IOs failure" in {
+        val effect               = Options.get(None)
+        val effectHandledToTries = optionsToTriesLayer.run(effect)
         assert {
             IOs.run(IOs.attempt(effectHandledToTries)) match
                 case Failure(err: Throwable) => err.getMessage == "missing value"
                 case _                       => false
         }
     }
-end layersTest
+end LayersTest
