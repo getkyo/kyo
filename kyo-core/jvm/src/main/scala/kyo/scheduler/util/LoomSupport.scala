0ca0dcde145a1007556af078c7a7ef8d5b533d6b
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2024-04-06 18:39:06 -0700

    Create `Unsafe` Logs API
    - Define effectful API in terms of unsafe

--- kyo-core/jvm/src/main/scala/kyo/scheduler/util/LoomSupport.scala
+++ kyo-core/jvm/src/main/scala/kyo/scheduler/util/LoomSupport.scala
@@ -25,7 +25,7 @@ object LoomSupport:
                 .invoke(null, factory).asInstanceOf[Executor]
         catch
             case ex if (NonFatal(ex)) =>
-                Logs.logger.warn(
+                Logs.unsafe.warn(
                     s"Warning: Kyo's Loom integration is unavailable: ${ex.getMessage()} " +
                         "For better performance, add '--add-opens=java.base/java.lang=ALL-UNNAMED' to " +
                         "your JVM arguments to use a dedicated thread pool. This step is needed due to " +
