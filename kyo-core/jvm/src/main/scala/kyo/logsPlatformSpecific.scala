16b8ef689e87ba228c9898f26202087f876b4a48
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2024-04-17 21:00:52 -0700

    fix SLF4J naming

--- kyo-core/jvm/src/main/scala/kyo/logsPlatformSpecific.scala
+++ kyo-core/jvm/src/main/scala/kyo/logsPlatformSpecific.scala
@@ -1,12 +1,12 @@
 package kyo
 trait logsPlatformSpecific:
-    val unsafe: Logs.Unsafe = logsPlatformSpecific.Unsafe.SL4J("kyo.logs")
+    val unsafe: Logs.Unsafe = logsPlatformSpecific.Unsafe.SLF4J("kyo.logs")
 
 object logsPlatformSpecific:
     object Unsafe:
-        object SL4J:
-            def apply(name: String) = new SL4J(org.slf4j.LoggerFactory.getLogger(name))
-        class SL4J(logger: org.slf4j.Logger) extends Logs.Unsafe:
+        object SLF4J:
+            def apply(name: String) = new SLF4J(org.slf4j.LoggerFactory.getLogger(name))
+        class SLF4J(logger: org.slf4j.Logger) extends Logs.Unsafe:
             inline def traceEnabled: Boolean = logger.isTraceEnabled
 
             inline def debugEnabled: Boolean = logger.isDebugEnabled
@@ -56,6 +56,6 @@ object logsPlatformSpecific:
             inline def error(msg: => String, t: => Throwable)(
                 using file: internal.Position
             ): Unit = if errorEnabled then logger.error(s"[$file] $msg", t)
-        end SL4J
+        end SLF4J
     end Unsafe
 end logsPlatformSpecific
