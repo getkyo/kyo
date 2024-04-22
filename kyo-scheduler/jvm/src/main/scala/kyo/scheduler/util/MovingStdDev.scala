2efa654b727b575111ebb3e21c654a920bf3e846
Flavio Brasil <fwbrasil@gmail.com>
2024-04-22 12:25:47 -0700

    improve moving stddev precision + introduce tests

--- kyo-scheduler/jvm/src/main/scala/kyo/scheduler/util/MovingStdDev.scala
+++ kyo-scheduler/jvm/src/main/scala/kyo/scheduler/util/MovingStdDev.scala
@@ -1,35 +1,40 @@
 package kyo.scheduler.util
 
-final private[kyo] class MovingStdDev(exp: Int):
-
-    private val window = 1 << exp
+final private[kyo] class MovingStdDev(window: Int):
     private val mask   = window - 1
     private val values = new Array[Long](window)
-    private val devs   = new Array[Long](window)
-    private var idx    = 0
-    private var sum    = 0L
-    private var sumDev = 0L
+    private var idx    = 0L
 
-    @volatile private var _avg = 0L
-    @volatile private var _dev = 0L
+    def dev(): Double =
+        val n = Math.min(idx, window.toLong)
+        if n <= 1 then return 0L
+        var sum   = 0L
+        var sumSq = 0L
+        var i     = 0
+        while i < n do
+            val value = values(i & mask)
+            sum += value
+            sumSq += value * value
+            i += 1
+        end while
+        val mean     = sum.toDouble / n
+        val variance = (sumSq.toDouble / n) - (mean * mean)
+        if n > 1 then Math.sqrt(variance * n / (n - 1)) else 0.0
+    end dev
 
-    def dev(): Long = _dev
-    def avg(): Long = _avg
+    def avg(): Double =
+        val n   = Math.min(idx, window.toLong)
+        var sum = 0d
+        var i   = 0
+        while i < n do
+            sum += values(i & mask)
+            i += 1
+        sum / n
+    end avg
 
     def observe(v: Long): Unit =
-        val prev = values(idx)
-        values(idx) = v
-        sum = sum - prev + v
-        _avg = sum >> exp
+        values((idx & mask).toInt) = v
+        idx += 1
 
-        val currDev = (v - _avg) * (v - _avg)
-        val prevDev = devs(idx)
-        devs(idx) = currDev
-        sumDev = sumDev - prevDev + currDev
-        _dev = Math.sqrt((sumDev >> exp).toDouble).toInt
-
-        idx = (idx + 1) & mask
-    end observe
-
-    override def toString = s"MovingStdDev(avg=${_avg},dev=${_dev})"
+    override def toString = s"MovingStdDev(avg=${avg()},dev=${dev()})"
 end MovingStdDev
