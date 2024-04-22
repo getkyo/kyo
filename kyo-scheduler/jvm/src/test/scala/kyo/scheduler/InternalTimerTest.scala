54b9ebe21be59a7d1964be1de28060c03f53c335
Flavio Brasil <fwbrasil@gmail.com>
2024-04-22 15:24:21 -0700

    internal timer test

---
+++ kyo-scheduler/jvm/src/test/scala/kyo/scheduler/InternalTimerTest.scala
@@ -0,0 +1,34 @@
+package kyo.scheduler
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import org.scalatest.NonImplicitAssertions
+import org.scalatest.freespec.AnyFreeSpec
+import scala.concurrent.duration.*
+
+class InternalTimerTest extends AnyFreeSpec with NonImplicitAssertions:
+
+    "schedule" in withExecutor { exec =>
+        val timer = InternalTimer(exec)
+        val cdl   = new CountDownLatch(1)
+        val task  = timer.schedule(1.nanos)(cdl.countDown())
+        cdl.await()
+        task.cancel()
+    }
+
+    "scheduleOnce" in withExecutor { exec =>
+        val timer = InternalTimer(exec)
+        val cdl   = new CountDownLatch(1)
+        val task  = timer.scheduleOnce(1.nanos)(cdl.countDown())
+        cdl.await()
+        task.cancel()
+    }
+
+    private def withExecutor[T](f: ScheduledExecutorService => T): T =
+        val exec = Executors.newSingleThreadScheduledExecutor()
+        try f(exec)
+        finally exec.shutdown()
+    end withExecutor
+
+end InternalTimerTest
