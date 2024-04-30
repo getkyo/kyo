445550fae9832dd6a15dc240c8591fb6b2dd28f9
Flavio Brasil <fwbrasil@gmail.com>
2024-04-29 21:04:32 -0700

    Aborts-like encoding for Vars

--- kyo-core/shared/src/test/scala/kyoTest/varsTest.scala
+++ kyo-core/shared/src/test/scala/kyoTest/varsTest.scala
@@ -5,42 +5,42 @@ import kyo.*
 class varsTest extends KyoTest:
 
     "get" in {
-        val r = Vars[Int].run(1)(Vars[Int].get.map(_ + 1)).pure
+        val r = Vars.run(1)(Vars.get[Int].map(_ + 1)).pure
         assert(r == 2)
     }
 
     "use" in {
-        val r = Vars[Int].run(1)(Vars[Int].use(_ + 1)).pure
+        val r = Vars.run(1)(Vars.use[Int](_ + 1)).pure
         assert(r == 2)
     }
 
     "set, get" in {
-        val r = Vars[Int].run(1)(Vars[Int].set(2).andThen(Vars[Int].get)).pure
+        val r = Vars.run(1)(Vars.set(2).andThen(Vars.get[Int])).pure
         assert(r == 2)
     }
 
     "get, set, get" in {
-        val r = Vars[Int].run(1)(
-            Vars[Int].get.map(i => Vars[Int].set(i + 1)).andThen(Vars[Int].get)
+        val r = Vars.run(1)(
+            Vars.get[Int].map(i => Vars.set[Int](i + 1)).andThen(Vars.get[Int])
         ).pure
         assert(r == 2)
     }
 
     "update" in {
-        val r = Vars[Int].run(1)(Vars[Int].update(_ + 1).andThen(Vars[Int].get)).pure
+        val r = Vars.run(1)(Vars.update[Int](_ + 1).andThen(Vars.get[Int])).pure
         assert(r == 2)
     }
 
     "nested let" in {
         IOs.run {
-            Vars[Int].run(1) {
+            Vars.run(1) {
                 IOs {
-                    Vars[Int].run(2) {
-                        Vars[Int].get.map { innerValue =>
+                    Vars.run(2) {
+                        Vars.get[Int].map { innerValue =>
                             assert(innerValue == 2)
                         }
                     }
-                }.unit.andThen(Vars[Int].get)
+                }.unit.andThen(Vars.get[Int])
                     .map { outerValue =>
                         assert(outerValue == 1)
                     }
@@ -49,30 +49,30 @@ class varsTest extends KyoTest:
     }
 
     "string value" in {
-        val result = Vars[String].run("a")(Vars[String].set("b").andThen(Vars[String].get)).pure
+        val result = Vars.run("a")(Vars.set("b").andThen(Vars.get[String])).pure
         assert(result == "b")
     }
 
     "side effect" in {
         var calls = 0
         val result =
-            Vars[Int].run(1) {
+            Vars.run(1) {
                 for
-                    _ <- Vars[Int].update { value =>
+                    _ <- Vars.update[Int] { value =>
                         calls += 1
                         value + 1
                     }
-                    result <- Vars[Int].get
+                    result <- Vars.get[Int]
                 yield result
             }.pure
         assert(result == 2 && calls == 1)
     }
 
     "inference" in {
-        val a: Int < Vars[Int]                   = Vars[Int].get
-        val b: Unit < (Vars[Int] & Vars[String]) = a.map(i => Vars[String].set(i.toString()))
-        val c: Unit < Vars[String]               = Vars[Int].run(1)(b)
-        val d: Unit < Any                        = Vars[String].run("t")(c)
+        val a: Int < Vars[Int]                   = Vars.get[Int]
+        val b: Unit < (Vars[Int] & Vars[String]) = a.map(i => Vars.set(i.toString()))
+        val c: Unit < Vars[String]               = Vars.run(1)(b)
+        val d: Unit < Any                        = Vars.run("t")(c)
         assert(d.pure == ())
     }
 end varsTest
