b17801756bdd90a8eb246a8ecbb25e3261801f64
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2024-04-30 19:55:00 -0700

    fix readme

--- kyo-tapir/src/main/scala/kyo/server/NettyKyoServer.scala
+++ kyo-tapir/src/main/scala/kyo/server/NettyKyoServer.scala
@@ -16,7 +16,6 @@ import kyo.internal.KyoSttpMonad
 import kyo.server.internal.KyoUtil.*
 import scala.concurrent.Future
 import scala.concurrent.duration.*
-import scala.concurrent.duration.Duration
 import scala.concurrent.duration.FiniteDuration
 import sttp.monad.MonadError
 import sttp.tapir.server.ServerEndpoint
