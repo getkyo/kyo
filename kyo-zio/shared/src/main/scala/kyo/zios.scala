1e92eb039da477ee038fb8af7e2bf5c821c517a6
Flavio Brasil <fwbrasil@gmail.com>
2024-05-04 18:28:31 -0700

    restructure ZIOs to provide better integration with Fibers and IOs

--- kyo-zio/shared/src/main/scala/kyo/zios.scala
+++ kyo-zio/shared/src/main/scala/kyo/zios.scala
@@ -1,20 +1,29 @@
 package kyo
 
+import ZIOs.internal.*
 import core.*
 import core.internal.*
 import kyo.fibersInternal.*
 import scala.util.control.NonFatal
 import zio.Task
-import zio.ZEnvironment
 import zio.ZIO
 
-class ZIOs extends Effect[ZIOs]:
-    type Command[T] = Task[T]
+opaque type ZIOs <: Fibers = Tasks & Fibers
 
-object ZIOs extends ZIOs:
+object ZIOs:
 
-    def run[T: Flat](v: T < (Fibers & ZIOs)): Task[T] =
-        def loop(v: T < (Fibers & ZIOs)): Task[T] =
+    def get[E >: Nothing: Tag, A](v: ZIO[Any, E, A]): A < (Aborts[E] & ZIOs) =
+        val task = v.fold[A < Aborts[E]](e => Aborts.fail(e), a => a)
+        Tasks.suspend[A < Aborts[E], A, Aborts[E]](task, identity)
+
+    def get[A](v: ZIO[Any, Nothing, A]): A < ZIOs =
+        Tasks.suspend[A](v)
+
+    inline def get[R: zio.Tag, A](v: ZIO[R, ?, A])(using tag: Tag[Envs[R]]): A < (Envs[R] & ZIOs) =
+        compiletime.error("ZIO environments are not supported yet. Please handle them before calling this method.")
+
+    def run[T: Flat](v: T < ZIOs): Task[T] =
+        def loop(v: T < ZIOs): Task[T] =
             v match
                 case kyo: Suspend[?, ?, ?, ?] =>
                     try
@@ -32,7 +41,7 @@ object ZIOs extends ZIOs:
                                         Left(ZIO.succeed(p.interrupt()))
                                     }
                             end match
-                        else if kyo.tag == Tag[ZIOs] then
+                        else if kyo.tag == Tag[Tasks] then
                             val k = kyo.asInstanceOf[Suspend[Task, Any, T, ZIOs]]
                             k.command.flatMap(v => loop(k(v)))
                         else
@@ -48,14 +57,9 @@ object ZIOs extends ZIOs:
         loop(v)
     end run
 
-    // TODO: fix Envs with type intersections
-    private[kyo] def get[R: zio.Tag, E: Tag, A](v: ZIO[R, E, A])(using tag: Tag[Envs[R]]): A < (Envs[R] & Aborts[E] & ZIOs) =
-        Envs.get[R](using tag).map(r => get(v.provideEnvironment(ZEnvironment(r))))
-
-    def get[E >: Nothing: Tag, A](v: ZIO[Any, E, A]): A < (Aborts[E] & ZIOs) =
-        val task = v.fold[A < Aborts[E]](e => Aborts.fail(e), a => a)
-        this.suspend(task, identity)
-
-    def get[A](v: ZIO[Any, Nothing, A]): A < ZIOs =
-        this.suspend(v)
+    private[kyo] object internal:
+        class Tasks extends Effect[Tasks]:
+            type Command[T] = Task[T]
+        object Tasks extends Tasks
+    end internal
 end ZIOs
