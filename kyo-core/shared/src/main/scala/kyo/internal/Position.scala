a9564b17694e89c085ff021c53774e221d5006d0
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2024-05-18 17:41:40 -0700

    Enable choosing a `Position` with/without owning scope

--- kyo-core/shared/src/main/scala/kyo/internal/Position.scala
+++ kyo-core/shared/src/main/scala/kyo/internal/Position.scala
@@ -9,12 +9,18 @@ object Position:
 
     inline given derive: Position = ${ Position.fileNameWithLine }
 
+    object WithOwner:
+        inline given derive: Position = ${ Position.withOwner }
+
     private def fileNameWithLine(using Quotes): Expr[Position] =
         val expansion = quotes.reflect.Position.ofMacroExpansion
         val name      = expansion.sourceFile.name
         val line      = expansion.startLine + 1
-        Expr(s"$name:$line - $owner")
+        Expr(s"$name:$line")
     end fileNameWithLine
+
+    private def withOwner(using Quotes): Expr[Position] =
+        Expr(s"${fileNameWithLine.valueOrAbort} - $owner")
 
     private def owner(using Quotes) =
         import quotes.reflect.*
