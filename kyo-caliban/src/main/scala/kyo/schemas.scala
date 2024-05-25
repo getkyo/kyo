d2ef03c4ddb6a1b46695a0f68102cef805c3794b
Pierre Ricadat <ghostdogpr@gmail.com>
2024-05-25 10:40:23 +0900

    WIP

--- kyo-caliban/src/main/scala/kyo/schemas.scala
+++ kyo-caliban/src/main/scala/kyo/schemas.scala
@@ -1,19 +1,17 @@
 package kyo
 
 import _root_.caliban.introspection.adt.__Type
+import _root_.caliban.schema.*
 import _root_.caliban.schema.Step.QueryStep
-import caliban.schema.Schema
-import caliban.schema.Step
 import kyo.*
 import zio.query.ZQuery
 
 given [R, T: Flat, S](using ev: Schema[R, T], ev2: (T < S) <:< (T < (Aborts[Throwable] & ZIOs))): Schema[R, T < S] =
     new Schema[R, T < S]:
-        override def optional: Boolean =
-            ev.optional
+        override def optional: Boolean = true
 
         override def toType(isInput: Boolean, isSubscription: Boolean): __Type =
             ev.toType_(isInput, isSubscription)
 
         override def resolve(value: T < S): Step[R] =
-            QueryStep(ZQuery.fromZIONow(ZIOs.run(ev2(value.map(ev.resolve)))))
+            QueryStep(ZQuery.fromZIONow(ZIOs.run(ev2(value).map(ev.resolve))))
--- kyo-caliban/src/test/scala/kyo/resolversTest.scala
+++ kyo-caliban/src/test/scala/kyo/resolversTest.scala
@@ -1,15 +1,43 @@
 package kyo
 
+import _root_.caliban.*
+import _root_.caliban.render
 import _root_.caliban.schema.Schema
 import kyo.*
 import kyoTest.KyoTest
+import zio.Task
 
 class resolversTest extends KyoTest:
+
+    def runZIO[T](v: Task[T]): T =
+        zio.Unsafe.unsafe(implicit u =>
+            zio.Runtime.default.unsafe.run(v).getOrThrow()
+        )
 
     case class Query(
         k1: Int < Aborts[Throwable],
         k2: Int < ZIOs,
-        k3: String < (Aborts[Throwable] & ZIOs)
+        k3: Int < (Aborts[Throwable] & ZIOs),
+        k4: Int < IOs
     ) derives Schema.SemiAuto
+
+    "schema derivation" in {
+        val expected = """type Query {
+                         |  k1: Int
+                         |  k2: Int
+                         |  k3: Int
+                         |  k4: Int
+                         |}""".stripMargin
+        assert(render[Query].trim == expected)
+    }
+
+    "execution" in runZIO {
+        val api = graphQL(RootResolver(Query(42, 42, 42, 42)))
+        for
+            interpreter <- api.interpreter
+            res         <- interpreter.execute("{ k1 k2 k3 k4 }")
+        yield assert(res.data.toString == """{"k1":42,"k2":42,"k3":42,"k4":42}""")
+        end for
+    }
 
 end resolversTest
