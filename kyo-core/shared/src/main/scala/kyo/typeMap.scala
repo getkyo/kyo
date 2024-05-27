9fd490f4d20f756cacb9ea95abdfe9e6af91b164
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2024-05-26 19:18:56 -0700

    rename: Env -> TypeMap

--- kyo-core/shared/src/main/scala/kyo/typeMap.scala
+++ kyo-core/shared/src/main/scala/kyo/typeMap.scala
@@ -3,19 +3,19 @@ package kyo
 import kyo.Tag.Intersection
 import scala.collection.immutable.HashMap
 
-opaque type Env[+R] = HashMap[Tag[?], Any]
-extension [R](self: Env[R])
+opaque type TypeMap[+R] = HashMap[Tag[?], Any]
+extension [R](self: TypeMap[R])
 
     private inline def fatal(using t: Tag[?]): Nothing =
-        throw new RuntimeException(s"fatal: kyo.Env of contents [$self] missing value of type: [${t.show}].")
+        throw new RuntimeException(s"fatal: kyo.TypeMap of contents [$self] missing value of type: [${t.show}].")
 
     inline def get[A >: R](using inline t: Tag[A]): A =
         self.getOrElse(t, fatal).asInstanceOf[A]
 
-    inline def add[A: Tag](a: A)(using inline t: Tag[A]): Env[R & A] =
+    inline def add[A: Tag](a: A)(using inline t: Tag[A]): TypeMap[R & A] =
         self.updated(t, a)
 
-    def union[R0](that: Env[R0]): Env[R & R0] =
+    def union[R0](that: TypeMap[R0]): TypeMap[R & R0] =
         self ++ that
 
     inline def size: Int        = self.size
@@ -24,15 +24,15 @@ extension [R](self: Env[R])
     private[kyo] def tag: Intersection[?] = Intersection(self.keySet.toIndexedSeq)
 end extension
 
-object Env:
-    val empty: Env[Any] = HashMap.empty
+object TypeMap:
+    val empty: TypeMap[Any] = HashMap.empty
 
-    def apply[A: Tag](a: A): Env[A] =
+    def apply[A: Tag](a: A): TypeMap[A] =
         HashMap(Tag[A] -> a)
-    def apply[A: Tag, B: Tag](a: A, b: B): Env[A & B] =
+    def apply[A: Tag, B: Tag](a: A, b: B): TypeMap[A & B] =
         HashMap(Tag[A] -> a, Tag[B] -> b)
-    def apply[A: Tag, B: Tag, C: Tag](a: A, b: B, c: C): Env[A & B & C] =
+    def apply[A: Tag, B: Tag, C: Tag](a: A, b: B, c: C): TypeMap[A & B & C] =
         HashMap(Tag[A] -> a, Tag[B] -> b, Tag[C] -> c)
-    def apply[A: Tag, B: Tag, C: Tag, D: Tag](a: A, b: B, c: C, d: D): Env[A & B & C & D] =
+    def apply[A: Tag, B: Tag, C: Tag, D: Tag](a: A, b: B, c: C, d: D): TypeMap[A & B & C & D] =
         HashMap(Tag[A] -> a, Tag[B] -> b, Tag[C] -> c, Tag[D] -> d)
-end Env
+end TypeMap
--- kyo-core/shared/src/test/scala/kyoTest/typeMapTest.scala
+++ kyo-core/shared/src/test/scala/kyoTest/typeMapTest.scala
@@ -2,55 +2,55 @@ package kyoTest
 
 import kyo.*
 
-class envTest extends KyoTest:
+class typeMapTest extends KyoTest:
     "empty" - {
-        "Env.empty" in {
-            assert(Env.empty.isEmpty)
+        "TypeMap.empty" in {
+            assert(TypeMap.empty.isEmpty)
         }
         "get[A]" in {
             assertDoesNotCompile(
                 """
-                  | Env.empty.get[String]
+                  | TypeMap.empty.get[String]
                   |""".stripMargin
             )
         }
     }
     "single" - {
-        "Env[String]" in {
-            val e = Env("Hello")
+        "TypeMap[String]" in {
+            val e = TypeMap("Hello")
             assert(e.get[String] == "Hello")
             assert(e.size == 1)
         }
-        "Env[Int]" in {
-            val e = Env(123)
+        "TypeMap[Int]" in {
+            val e = TypeMap(123)
             assert(e.get[Int] == 123)
             assert(e.size == 1)
         }
-        "Env[trait]" in {
+        "TypeMap[trait]" in {
             trait A
             given CanEqual[A, A] = CanEqual.derived
             val a                = new A {}
-            val e                = Env(a)
+            val e                = TypeMap(a)
             assert(e.get[A] == a)
             assert(e.size == 1)
         }
     }
     "intersection" - {
         "two" in {
-            val e = Env("Hello", 123)
+            val e = TypeMap("Hello", 123)
             assert(e.get[String] == "Hello")
             assert(e.get[Int] == 123)
             assert(e.size == 2)
         }
         "three" in {
-            val e = Env("Hello", 123, true)
+            val e = TypeMap("Hello", 123, true)
             assert(e.get[String] == "Hello")
             assert(e.get[Int] == 123)
             assert(e.get[Boolean])
             assert(e.size == 3)
         }
         "four" in {
-            val e = Env("Hello", 123, true, 'c')
+            val e = TypeMap("Hello", 123, true, 'c')
             assert(e.get[String] == "Hello")
             assert(e.get[Int] == 123)
             assert(e.get[Boolean])
@@ -58,44 +58,44 @@ class envTest extends KyoTest:
             assert(e.size == 4)
         }
         "distinct" in pendingUntilFixed {
-            assertDoesNotCompile("Env(0, 0)")
+            assertDoesNotCompile("TypeMap(0, 0)")
         }
     }
     "fatal" - {
         import scala.util.Try
         import scala.util.Failure
 
-        def test[T: Tag](e: Env[T], contents: String, tpe: String) =
+        def test[T: Tag](e: TypeMap[T], contents: String, tpe: String) =
             Try(e.get[T]) match
                 case Failure(error) => assert(
-                        error.getMessage == s"fatal: kyo.Env of contents [$contents] missing value of type: [$tpe]."
+                        error.getMessage == s"fatal: kyo.TypeMap of contents [$contents] missing value of type: [$tpe]."
                     )
                 case _ => fail("Expected error")
 
         "empty" in {
-            val e: Env[Boolean] = Env.empty.asInstanceOf[Env[Boolean]]
+            val e: TypeMap[Boolean] = TypeMap.empty.asInstanceOf[TypeMap[Boolean]]
             test(e, "HashMap()", "scala.Boolean")
         }
         "non-empty" in {
-            val e: Env[String & Boolean] = Env[Boolean](true).asInstanceOf[Env[String & Boolean]]
+            val e: TypeMap[String & Boolean] = TypeMap[Boolean](true).asInstanceOf[TypeMap[String & Boolean]]
             test[String](e, """HashMap(Tag(-4scala.Boolean;,%scala.AnyVal;"b!M;"V!A;) -> true)""", "java.lang.String")
         }
     }
 
     "add" - {
-        "Env[Int] -> Env[Int & Boolean]" in {
-            val e1: Env[Int]           = Env(42)
-            val e2: Env[Int & Boolean] = e1.add(true)
+        "TypeMap[Int] -> TypeMap[Int & Boolean]" in {
+            val e1: TypeMap[Int]           = TypeMap(42)
+            val e2: TypeMap[Int & Boolean] = e1.add(true)
             assert(e2.get[Int] == 42)
             assert(e2.get[Boolean])
         }
     }
 
     "union" - {
-        "Env[Int] + Env[Char] -> Env[Int & Char]" in {
-            val e1: Env[Int]        = Env(42)
-            val e2: Env[Char]       = Env('c')
-            val e3: Env[Int & Char] = e1.union(e2)
+        "TypeMap[Int] + TypeMap[Char] -> TypeMap[Int & Char]" in {
+            val e1: TypeMap[Int]        = TypeMap(42)
+            val e2: TypeMap[Char]       = TypeMap('c')
+            val e3: TypeMap[Int & Char] = e1.union(e2)
             assert(e3.get[Int] == 42)
             assert(e3.get[Char] == 'c')
         }
@@ -105,7 +105,7 @@ class envTest extends KyoTest:
         "Env[Int & String] -> Env[Int]" in pendingUntilFixed {
             assertCompiles(
                 """
-                  | val e = Env(42, "")
+                  | val e = TypeMap(42, "")
                   | val p = e.prune[Int]
                   | assert(p.size == 1)
                   |""".stripMargin
@@ -113,4 +113,4 @@ class envTest extends KyoTest:
         }
     }
 
-end envTest
+end typeMapTest
