f2e38272d972099f8d2a65ecee9e1c65667b7f32
Flavio Brasil <fwbrasil@gmail.com>
2024-06-05 18:31:04 -0700

    remove separate kyo-os-lib module

--- kyo-core/jvm/src/main/scala/kyo/Process.scala
+++ kyo-core/jvm/src/main/scala/kyo/Process.scala
@@ -11,168 +11,6 @@ import kyo.*
 import kyo.internal.Trace
 import scala.jdk.CollectionConverters.*
 
-sealed trait ProcessCommand:
-    self =>
-    import ProcessCommand.*
-
-    /** Spawns a new process executing this command
-      */
-    def spawn(using Trace): Process < IOs
-
-    /** Spawns a new process and returns its output as a String
-      */
-    def text(using Trace): String < IOs =
-        stream.map { inputStream =>
-            new String(inputStream.readAllBytes())
-        }
-
-    /** Spawns a new process and returns its output as a Stream
-      */
-    def stream(using Trace): InputStream < IOs =
-        spawn.map(_.stdout)
-
-    def exitValue(using Trace): Int < IOs =
-        spawn.map(_.exitValue)
-
-    def waitFor(using Trace): Int < IOs =
-        spawn.map(_.waitFor)
-
-    def waitFor(timeout: Long, unit: TimeUnit)(using Trace): Boolean < IOs =
-        spawn.map(_.waitFor(timeout, unit))
-
-    def pipe(that: ProcessCommand): ProcessCommand =
-        ProcessCommand.Piped(self.flatten ++ that.flatten).simplify
-
-    def andThen(that: ProcessCommand): ProcessCommand =
-        self.pipe(that)
-
-    def flatten: List[ProcessCommand.Simple]
-
-    def +(that: ProcessCommand): ProcessCommand =
-        val cs = self.flatten ++ that.flatten
-        if cs.length > 1 then ProcessCommand.Piped(cs) else cs.head
-
-    def map(f: ProcessCommand.Simple => ProcessCommand): ProcessCommand
-
-    def simplify: ProcessCommand =
-        self match
-            case ProcessCommand.Piped(cs) if cs.length == 1 => cs.head
-            case c                                          => c
-
-    /** Modifies the current working directory of the command
-      */
-    def cwd(newCwd: Path): ProcessCommand
-
-    /** Modifies the environment variables of the command
-      */
-    def env(newEnv: Map[String, String]): ProcessCommand
-
-    /** Modifies the `stdin` of the command
-      */
-    def stdin(newStdin: ProcessInput): ProcessCommand
-
-    /** Modifies the `stdout` of the command
-      */
-    def stdout(newStdout: ProcessOutput): ProcessCommand
-
-    /** Modifies the `stderr` of the command
-      */
-    def stderr(newStderr: ProcessOutput): ProcessCommand
-
-    /** Changes `redirectErrorStream` property
-      */
-    def redirectErrorStream(value: Boolean): ProcessCommand
-
-end ProcessCommand
-
-object ProcessCommand:
-
-    def apply(commands: String*): ProcessCommand.Simple =
-        ProcessCommand.Simple(commands.toList)
-
-    case class Simple(
-        command: List[String],
-        cwd: Option[Path] = None,
-        env: Map[String, String] = Map.empty,
-        stdin: ProcessInput = ProcessInput.Inherit,
-        stdout: ProcessOutput = ProcessOutput.Pipe,
-        stderr: ProcessOutput = ProcessOutput.Pipe,
-        redirectErrorStream: Boolean = false
-    ) extends ProcessCommand:
-        self =>
-        override def spawn(using Trace): Process < IOs =
-            for
-                process <- IOs {
-                    val builder = new ProcessBuilder(command*)
-
-                    builder.redirectErrorStream(redirectErrorStream)
-                    cwd.map(p => builder.directory(p.toFile()))
-
-                    if env.nonEmpty then
-                        builder.environment().putAll(env.asJava)
-
-                    stdin match
-                        case ProcessInput.Stream(_) => ()
-                        case _                      => builder.redirectInput(ProcessInput.redirect(stdin))
-                    builder.redirectOutput(ProcessOutput.redirect(stdout))
-                    builder.redirectError(ProcessOutput.redirect(stderr))
-
-                    Process(builder.start())
-                }
-                _ <- stdin match
-                    case ProcessInput.Stream(stream) =>
-                        val resources = Resources.acquireRelease((stream, process.stdin)) { streams =>
-                            streams._1.close()
-                            streams._2.close()
-                        }.map { streams =>
-                            streams._1.transferTo(streams._2)
-                            ()
-                        }
-                        for
-                            _ <- Fibers.init(Resources.run(resources))
-                        yield ()
-                    case _ => IOs.unit
-            yield process
-
-        override def cwd(newCwd: Path): ProcessCommand                   = self.copy(cwd = Some(newCwd))
-        override def env(newEnv: Map[String, String]): ProcessCommand    = self.copy(env = newEnv)
-        override def stdin(newStdin: ProcessInput): ProcessCommand       = self.copy(stdin = newStdin)
-        override def stdout(newStdout: ProcessOutput): ProcessCommand    = self.copy(stdout = newStdout)
-        override def stderr(newStderr: ProcessOutput): ProcessCommand    = self.copy(stderr = newStderr)
-        override def redirectErrorStream(value: Boolean): ProcessCommand = self.copy(redirectErrorStream = value)
-
-        override def flatten: List[ProcessCommand.Simple]                            = List(self)
-        override def map(f: ProcessCommand.Simple => ProcessCommand): ProcessCommand = f(self)
-
-    end Simple
-
-    case class Piped(commands: List[ProcessCommand.Simple]) extends ProcessCommand:
-        self =>
-        def spawnAll(using Trace): List[Process] < IOs =
-            if commands.isEmpty then IOs(List.empty)
-            else
-                commands.tail.foldLeft(commands.head.spawn.map(p => (p :: Nil, p.stdout))) { case (acc, nextCommand) =>
-                    for
-                        a <- acc
-                        (processes, lastStdout) = a
-                        nextProcess <- nextCommand.stdin(ProcessInput.Stream(lastStdout)).spawn
-                    yield (processes ++ List(nextProcess), nextProcess.stdout)
-                }.map(_._1)
-        override def spawn(using Trace): Process < IOs =
-            spawnAll.map(_.last)
-
-        override def cwd(newCwd: Path): ProcessCommand                   = self.map(_.copy(cwd = Some(newCwd)))
-        override def env(newEnv: Map[String, String]): ProcessCommand    = self.map(_.copy(env = newEnv))
-        override def stdin(newStdin: ProcessInput): ProcessCommand       = self.map(_.copy(stdin = newStdin))
-        override def stdout(newStdout: ProcessOutput): ProcessCommand    = self.map(_.copy(stdout = newStdout))
-        def stderr(newStderr: ProcessOutput): ProcessCommand             = self.map(_.copy(stderr = newStderr))
-        override def redirectErrorStream(value: Boolean): ProcessCommand = self.map(_.copy(redirectErrorStream = value))
-        override def flatten: List[ProcessCommand.Simple]                = commands
-        override def map(f: ProcessCommand.Simple => ProcessCommand): ProcessCommand =
-            ProcessCommand.Piped(commands.map(f).map(_.flatten).flatten)
-    end Piped
-end ProcessCommand
-
 case class Process(private val process: JProcess):
     def stdin: OutputStream                                                = process.getOutputStream
     def stdout: InputStream                                                = process.getInputStream
@@ -185,30 +23,213 @@ case class Process(private val process: JProcess):
     def isAlive(using Trace): Boolean < IOs                                = IOs(process.isAlive())
 end Process
 
-sealed trait ProcessOutput derives CanEqual
-object ProcessOutput:
-    case class FileRedirect(file: File)       extends ProcessOutput
-    case class FileAppendRedirect(file: File) extends ProcessOutput
-    case object Inherit                       extends ProcessOutput
-    case object Pipe                          extends ProcessOutput
-    private[kyo] def redirect(std: ProcessOutput): Redirect =
-        std match
-            case ProcessOutput.FileRedirect(file)       => Redirect.to(file)
-            case ProcessOutput.FileAppendRedirect(file) => Redirect.appendTo(file)
-            case ProcessOutput.Inherit                  => Redirect.INHERIT
-            case ProcessOutput.Pipe                     => Redirect.PIPE
-end ProcessOutput
+object Process:
 
-sealed trait ProcessInput derives CanEqual
-object ProcessInput:
-    case object Inherit                    extends ProcessInput
-    case object Pipe                       extends ProcessInput
-    case class Stream(stream: InputStream) extends ProcessInput
-    private[kyo] def redirect(std: ProcessInput): Redirect =
-        std match
-            case ProcessInput.Inherit => Redirect.INHERIT
-            case ProcessInput.Pipe    => Redirect.PIPE
-            case _                    => null
-    def fromString(s: String): ProcessInput                   = fromString(s, StandardCharsets.UTF_8)
-    def fromString(s: String, charset: Charset): ProcessInput = Stream(new ByteArrayInputStream(s.getBytes(charset)))
-end ProcessInput
+    object jvm:
+        /** Executes a class with given args for the JVM.
+          */
+        def spawn(clazz: Class[?], args: List[String] = Nil)(using Trace): Process < IOs =
+            command(clazz, args).map(_.spawn)
+
+        /** Returns a `Process.Command` representing the execution of the `clazz` Class in a new JVM process. To finally execute the
+          * command, use `spawn` or use directly `jvm.spawn`.
+          */
+        def command(clazz: Class[?], args: List[String] = Nil): Process.Command < IOs =
+            IOs {
+                val javaHome  = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java"
+                val classPath = System.getProperty("java.class.path")
+                val command   = javaHome :: "-cp" :: classPath :: clazz.getName().init :: args
+
+                Process.Command(command*)
+            }
+    end jvm
+
+    sealed trait Output derives CanEqual
+    object Output:
+        case class FileRedirect(file: File)       extends Output
+        case class FileAppendRedirect(file: File) extends Output
+        case object Inherit                       extends Output
+        case object Pipe                          extends Output
+        private[kyo] def redirect(std: Output): Redirect =
+            std match
+                case Output.FileRedirect(file)       => Redirect.to(file)
+                case Output.FileAppendRedirect(file) => Redirect.appendTo(file)
+                case Output.Inherit                  => Redirect.INHERIT
+                case Output.Pipe                     => Redirect.PIPE
+    end Output
+
+    sealed trait Input derives CanEqual
+    object Input:
+        case object Inherit                    extends Input
+        case object Pipe                       extends Input
+        case class Stream(stream: InputStream) extends Input
+        private[kyo] def redirect(std: Input): Redirect =
+            std match
+                case Input.Inherit => Redirect.INHERIT
+                case Input.Pipe    => Redirect.PIPE
+                case _             => null
+        def fromString(s: String): Input                   = fromString(s, StandardCharsets.UTF_8)
+        def fromString(s: String, charset: Charset): Input = Stream(new ByteArrayInputStream(s.getBytes(charset)))
+    end Input
+
+    sealed trait Command:
+        self =>
+        import Command.*
+
+        /** Spawns a new process executing this command
+          */
+        def spawn(using Trace): Process < IOs
+
+        /** Spawns a new process and returns its output as a String
+          */
+        def text(using Trace): String < IOs =
+            stream.map { inputStream =>
+                new String(inputStream.readAllBytes())
+            }
+
+        /** Spawns a new process and returns its output as a Stream
+          */
+        def stream(using Trace): InputStream < IOs =
+            spawn.map(_.stdout)
+
+        def exitValue(using Trace): Int < IOs =
+            spawn.map(_.exitValue)
+
+        def waitFor(using Trace): Int < IOs =
+            spawn.map(_.waitFor)
+
+        def waitFor(timeout: Long, unit: TimeUnit)(using Trace): Boolean < IOs =
+            spawn.map(_.waitFor(timeout, unit))
+
+        def pipe(that: Command): Command =
+            Piped(self.flatten ++ that.flatten).simplify
+
+        def andThen(that: Command): Command =
+            self.pipe(that)
+
+        def flatten: List[Simple]
+
+        def +(that: Command): Command =
+            val cs = self.flatten ++ that.flatten
+            if cs.length > 1 then Piped(cs) else cs.head
+
+        def map(f: Simple => Command): Command
+
+        def simplify: Command =
+            self match
+                case Piped(cs) if cs.length == 1 => cs.head
+                case c                           => c
+
+        /** Modifies the current working directory of the command
+          */
+        def cwd(newCwd: Path): Command
+
+        /** Modifies the environment variables of the command
+          */
+        def env(newEnv: Map[String, String]): Command
+
+        /** Modifies the `stdin` of the command
+          */
+        def stdin(newStdin: Input): Command
+
+        /** Modifies the `stdout` of the command
+          */
+        def stdout(newStdout: Output): Command
+
+        /** Modifies the `stderr` of the command
+          */
+        def stderr(newStderr: Output): Command
+
+        /** Changes `redirectErrorStream` property
+          */
+        def redirectErrorStream(value: Boolean): Command
+
+    end Command
+
+    object Command:
+
+        def apply(commands: String*): Simple =
+            Simple(commands.toList)
+
+        case class Simple(
+            command: List[String],
+            cwd: Option[Path] = None,
+            env: Map[String, String] = Map.empty,
+            stdin: Input = Input.Inherit,
+            stdout: Output = Output.Pipe,
+            stderr: Output = Output.Pipe,
+            redirectErrorStream: Boolean = false
+        ) extends Command:
+            self =>
+            override def spawn(using Trace): Process < IOs =
+                for
+                    process <- IOs {
+                        val builder = new ProcessBuilder(command*)
+
+                        builder.redirectErrorStream(redirectErrorStream)
+                        cwd.map(p => builder.directory(p.toFile()))
+
+                        if env.nonEmpty then
+                            builder.environment().putAll(env.asJava)
+
+                        stdin match
+                            case Input.Stream(_) => ()
+                            case _               => builder.redirectInput(Input.redirect(stdin))
+                        builder.redirectOutput(Output.redirect(stdout))
+                        builder.redirectError(Output.redirect(stderr))
+
+                        Process(builder.start())
+                    }
+                    _ <- stdin match
+                        case Input.Stream(stream) =>
+                            val resources = Resources.acquireRelease((stream, process.stdin)) { streams =>
+                                streams._1.close()
+                                streams._2.close()
+                            }.map { streams =>
+                                streams._1.transferTo(streams._2)
+                                ()
+                            }
+                            for
+                                _ <- Fibers.init(Resources.run(resources))
+                            yield ()
+                        case _ => IOs.unit
+                yield process
+
+            override def cwd(newCwd: Path)                   = self.copy(cwd = Some(newCwd))
+            override def env(newEnv: Map[String, String])    = self.copy(env = newEnv)
+            override def stdin(newStdin: Input)              = self.copy(stdin = newStdin)
+            override def stdout(newStdout: Output)           = self.copy(stdout = newStdout)
+            override def stderr(newStderr: Output)           = self.copy(stderr = newStderr)
+            override def redirectErrorStream(value: Boolean) = self.copy(redirectErrorStream = value)
+
+            override def flatten: List[Simple]              = List(self)
+            override def map(f: Simple => Command): Command = f(self)
+
+        end Simple
+
+        case class Piped(commands: List[Simple]) extends Command:
+            self =>
+            def spawnAll(using Trace): List[Process] < IOs =
+                if commands.isEmpty then IOs(List.empty)
+                else
+                    commands.tail.foldLeft(commands.head.spawn.map(p => (p :: Nil, p.stdout))) { case (acc, nextCommand) =>
+                        for
+                            a <- acc
+                            (processes, lastStdout) = a
+                            nextProcess <- nextCommand.stdin(Input.Stream(lastStdout)).spawn
+                        yield (processes ++ List(nextProcess), nextProcess.stdout)
+                    }.map(_._1)
+            override def spawn(using Trace): Process < IOs =
+                spawnAll.map(_.last)
+
+            override def cwd(newCwd: Path)                   = self.map(_.copy(cwd = Some(newCwd)))
+            override def env(newEnv: Map[String, String])    = self.map(_.copy(env = newEnv))
+            override def stdin(newStdin: Input)              = self.map(_.copy(stdin = newStdin))
+            override def stdout(newStdout: Output)           = self.map(_.copy(stdout = newStdout))
+            def stderr(newStderr: Output)                    = self.map(_.copy(stderr = newStderr))
+            override def redirectErrorStream(value: Boolean) = self.map(_.copy(redirectErrorStream = value))
+            override def flatten: List[Simple]               = commands
+            override def map(f: Simple => Command)           = Piped(commands.map(f).map(_.flatten).flatten)
+        end Piped
+    end Command
+end Process
--- kyo-core/jvm/src/test/scala/kyoTest/ProcessTest.scala
+++ kyo-core/jvm/src/test/scala/kyoTest/ProcessTest.scala
@@ -1,38 +1,38 @@
-package kyo
+package kyoTest
 
 import java.io.*
 import java.nio.charset.StandardCharsets
 import kyo.*
 
-class osTest extends KyoTest:
+class ProcessTest extends KyoTest:
 
     "execute command" in run {
         assert {
-            IOs.run(ProcessCommand("echo", "-n", "some string").text).pure == "some string"
+            IOs.run(Process.Command("echo", "-n", "some string").text).pure == "some string"
         }
     }
     "stream as stdin" in run {
         val stream = new ByteArrayInputStream("some string".getBytes(StandardCharsets.UTF_8))
-        val c      = ProcessCommand("cat").stdin(ProcessInput.Stream(stream))
+        val c      = Process.Command("cat").stdin(Process.Input.Stream(stream))
 
         assert {
             IOs.run(c.text).pure == "some string"
         }
     }
     "piped commands" in run {
-        val c = ProcessCommand("echo", "-n", "some string").andThen(ProcessCommand("cat"))
+        val c = Process.Command("echo", "-n", "some string").andThen(Process.Command("cat"))
         assert(IOs.run(c.text).pure == "some string")
     }
     "piped commands with stream stdin" in run {
         val stream = new ByteArrayInputStream("2\n1".getBytes(StandardCharsets.UTF_8))
-        val c0     = ProcessCommand("cat").andThen(ProcessCommand("sort"))
-        val c      = c0.stdin(ProcessInput.Stream(stream))
+        val c0     = Process.Command("cat").andThen(Process.Command("sort"))
+        val c      = c0.stdin(Process.Input.Stream(stream))
         assert(IOs.run(c.text).pure == "1\n2\n")
     }
 
     "read stdout to stream" in run {
         val res = IOs.run {
-            ProcessCommand("echo", "-n", "some string").stream.map { s =>
+            Process.Command("echo", "-n", "some string").stream.map { s =>
                 val arr = new Array[Byte](11)
                 s.read(arr)
                 arr
@@ -43,9 +43,29 @@ class osTest extends KyoTest:
         }
     }
     "bad exit code" in run {
-        assert(IOs.run(ProcessCommand("ls", "--wrong-flag").waitFor).pure != 0)
+        assert(IOs.run(Process.Command("ls", "--wrong-flag").waitFor).pure != 0)
     }
     "good exit code" in run {
-        assert(IOs.run(ProcessCommand("echo", "some string").waitFor).pure == 0)
+        assert(IOs.run(Process.Command("echo", "some string").waitFor).pure == 0)
     }
-end osTest
+
+    "jvm" - {
+        "execute new jvm and throw error" in run {
+            assert {
+                IOs.run(Process.jvm.command(classOf[TestMainClass.type], "some-arg" :: Nil).map(_.waitFor)).pure == 1
+            }
+        }
+
+        "execute new jvm and end without error" in run {
+            assert {
+                IOs.run(Process.jvm.command(classOf[TestMainClass.type]).map(_.waitFor)).pure == 0
+            }
+        }
+    }
+end ProcessTest
+
+object TestMainClass:
+    def main(args: Array[String]) =
+        if args.isEmpty then System.exit(0)
+        else System.exit(1)
+end TestMainClass
