f74c6ee17db44b4ce404fb9c2c771da805da00d7
Jason Pickens <jasonpickensnz@gmail.com>
2024-06-24 06:20:48 +1200

    Add test for aborts

--- kyo-grpc/e2e/shared/src/main/scala/kyo/grpc/TestServiceImpl.scala
+++ kyo-grpc/e2e/shared/src/main/scala/kyo/grpc/TestServiceImpl.scala
@@ -2,15 +2,15 @@ package kyo.grpc
 
 import io.grpc.*
 import kyo.*
-import kyo.grpc.test.*
-import kyo.grpc.test.given
+import kyo.grpc.test.{*, given}
 
 object TestServiceImpl extends TestService:
     override def unary(request: Request): Response < GrpcResponses =
         request match
             case Request.Empty => IOs.fail("Empty request")
             case nonEmpty: Request.NonEmpty => nonEmpty match
-                case Echo(message, _) => EchoEcho(message)
-                case Fail(code, _) => Aborts.fail(Status.fromCodeValue(code).asException)
+                    case Echo(message, _) => EchoEcho(message)
+                    case Abort(code, _)   => Aborts.fail(Status.fromCodeValue(code).asException)
+                    case Fail(message, _) => IOs.fail(message)
 
 end TestServiceImpl
--- kyo-grpc/e2e/shared/src/test/scala/kyo/grpc/ServiceTest.scala
+++ kyo-grpc/e2e/shared/src/test/scala/kyo/grpc/ServiceTest.scala
@@ -3,21 +3,47 @@ package kyo.grpc
 import io.grpc.*
 import kyo.*
 import kyo.grpc.test.*
+import org.scalatest.EitherValues.*
+import org.scalatest.Inspectors.*
 
 import java.net.ServerSocket
 import java.util.concurrent.TimeUnit
 
 class ServiceTest extends KyoTest:
 
-    private given CanEqual[Response, EchoEcho] = CanEqual.derived
+    private given CanEqual[Response, EchoEcho]       = CanEqual.derived
+    private given CanEqual[Status, Status]           = CanEqual.derived
+    private given CanEqual[Status.Code, Status.Code] = CanEqual.derived
 
     "echo" in run {
         for
-            port     <- findFreePort
-            _        <- createServer(port)
-            client   <- createClient(port)
-            response <- Fibers.fromFuture(client.unary(Echo("Hello")))
+            port   <- findFreePort
+            _      <- createServer(port)
+            client <- createClient(port)
+            request = Echo("Hello")
+            response <- Fibers.fromFuture(client.unary(request))
         yield assert(response == EchoEcho("Hello"))
+    }
+
+    "fail" in {
+        forEvery(Status.Code.values().filterNot(_ == Status.Code.OK)) { code =>
+            run {
+                for
+                    port   <- findFreePort
+                    _      <- createServer(port)
+                    client <- createClient(port)
+                    status  = code.toStatus
+                    request = Abort(status.getCode.value)
+                    response <- IOs.catching(Fibers.fromFuture(client.unary(request))) {
+                        case e: StatusRuntimeException => e
+                    }
+                yield
+                    val responseOrException = response match
+                        case e: StatusRuntimeException => Right(e)
+                        case other                     => Left(other)
+                    assert(responseOrException.value.getStatus == status)
+            }
+        }
     }
 
     private def createServer(port: Int) =
