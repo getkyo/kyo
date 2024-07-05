39073dbfd12263581770931f18d6ad1bf3ad9ec0
Flavio Brasil <fwbrasil@gmail.com>
2024-07-04 17:02:49 -0700

    avoid Reducible allocations

--- kyo-prelude/shared/src/main/scala/kyo2/kernel/Reducible.scala
+++ kyo-prelude/shared/src/main/scala/kyo2/kernel/Reducible.scala
@@ -5,15 +5,20 @@ sealed trait Reducible[S]:
     def apply[A, S1](value: A < (S1 & S)): A < (S1 & SReduced)
 
 sealed trait LowPriorityReducibles:
-    given irreducible[S]: Reducible[S] with
-        type SReduced = S
-        override def apply[A, S1](value: A < (S1 & S)): A < (S1 & S) = value
-end LowPriorityReducibles
+    inline given irreducible[S]: Reducible.Aux[S, S] =
+        Reducible.cached.asInstanceOf[Reducible.Aux[S, S]]
 
 object Reducible extends LowPriorityReducibles:
     trait Eliminable[S]
 
-    given eliminate[S](using Eliminable[S]): Reducible[S] with
-        type SReduced = Any
-        def apply[A, S1](value: A < (S1 & S)): A < S1 = value.asInstanceOf[A < S1]
+    private[kernel] val cached =
+        new Reducible[Any]:
+            type SReduced = Any
+            def apply[A, S1](value: A < (S1 & Any)) = value.asInstanceOf[A < (S1 & SReduced)]
+
+    type Aux[S, R] = Reducible[S] { type SReduced = R }
+
+    inline given eliminate[S](using Eliminable[S]): Aux[S, Any] =
+        cached.asInstanceOf[Aux[S, Any]]
+
 end Reducible
