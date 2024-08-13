4aeb006b9949041e48d800b9e035fe6b68b8b5ae
Flavio Brasil <fwbrasil@gmail.com>
2024-08-13 14:11:03 -0700

    fixing file naming

--- kyo-core/js/src/main/scala/kyo/AsyncStubs.scala
+++ kyo-core/js/src/main/scala/kyo/AsyncStubs.scala
@@ -2,7 +2,6 @@ package java.util.concurrent.locks
 
 object LockSupport:
     private def fail =
-        (new Exception).printStackTrace()
         throw new UnsupportedOperationException("fiber.block is not supported in ScalaJS")
     def park(o: Object): Unit         = fail
     def parkNanos(o: Object, l: Long) = fail
