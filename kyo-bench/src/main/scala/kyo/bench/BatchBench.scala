9fa0b8ad6b1833aee4fc0d932fb0d5e5b9aec05f
Flavio Brasil <fwbrasil@users.noreply.github.com>
2024-10-06 20:17:45 -0700

    bench: Batch (#718)
    Initial step to optimize the `Batch` effect. It's a port of a
    [benchmark](https://github.com/zio/zio-query/blob/0ea6d62fc482f0f03ee5419af2899f6d1531836d/benchmarks/src/main/scala/zio/query/DataSourceBenchmark.scala#L1)
    in `zio-query`. [Current
    results](https://jmh.morethan.io/?source=https://gist.githubusercontent.com/fwbrasil/4192850428178d3dc0f4842a0b1bdf5b/raw/4921d952bb86be51d55537973d09ec0d6f14be4a/jmh-result.json):
    
    
    ![image](https://github.com/user-attachments/assets/63480281-971d-40d2-93ff-911e56afe6e6)
    
    I''m not sure why cats-effect/Fetch are so behind. Looking at the
    profiling session it seems due to a large number of allocations building
    hash sets:
    
    
    ![flame_graph](https://github.com/user-attachments/assets/715b0b4c-38a9-4bce-a858-cfcaf5025c88)

---
+++ kyo-bench/src/main/scala/kyo/bench/BatchBench.scala
@@ -0,0 +1,69 @@
+package kyo.bench
+
+import java.util.concurrent.locks.LockSupport
+
+class BatchBench extends Bench.SyncAndFork(5050):
+
+    val count = 100
+
+    object CatsImpl:
+        import cats.data.NonEmptyList
+        import cats.effect.*
+        import fetch.*
+
+        object PlusOne extends Data[Int, Int]:
+            def name = "PlusOne"
+
+            val source: DataSource[IO, Int, Int] = new DataSource[IO, Int, Int]:
+                override def data: Data[Int, Int] = PlusOne
+
+                override def CF: Concurrent[IO] = Concurrent[IO]
+
+                override def fetch(id: Int): IO[Option[Int]] =
+                    IO(Some(id + 1))
+
+                override def batch(ids: NonEmptyList[Int]): IO[Map[Int, Int]] =
+                    IO(ids.toList.view.map(id => id -> (id + 1)).toMap)
+        end PlusOne
+
+        def fetchPlusOne(n: Int): Fetch[IO, Int] =
+            Fetch(n, PlusOne.source)
+    end CatsImpl
+
+    def catsBench() =
+        import cats.effect.*
+        import CatsImpl.*
+        import fetch.*
+        import cats.syntax.all.*
+
+        val reqs  = (0 until count).toList.map(fetchPlusOne)
+        val query = reqs.sequence[Fetch[IO, *], Int].map(_.sum)
+        Fetch.run(query)
+    end catsBench
+
+    object KyoImpl:
+        import kyo.*
+        val source = Batch.source[Int, Int, Any]((seq: Seq[Int]) => ((key: Int) => key + 1): (Int => Int < Any))
+
+    def kyoBench() =
+        import kyo.*
+        import KyoImpl.*
+        Batch.run(Batch.foreach(0 until count)(source)).map(_.sum)
+    end kyoBench
+
+    object ZIOImpl:
+        import zio.*
+        import zio.query.*
+        case class Req(i: Int) extends Request[Nothing, Int]
+        val ds = DataSource.fromFunctionBatchedZIO("PlusOne") { (reqs: Chunk[Req]) => ZIO.succeed(reqs.map(_.i + 1)) }
+    end ZIOImpl
+
+    def zioBench() =
+        import zio.*
+        import zio.query.*
+        import ZIOImpl.*
+
+        val reqs = (0 until count).toList.map(i => ZQuery.fromRequest(Req(i))(ds))
+        ZQuery.collectAllBatched(reqs).map(_.sum).run
+    end zioBench
+end BatchBench
