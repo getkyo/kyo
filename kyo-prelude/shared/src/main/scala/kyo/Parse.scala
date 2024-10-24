a85cf86f8df61874b6b3aa0f3f796dc5ad115c52
Flavio Brasil <fwbrasil@gmail.com>
2024-10-23 18:05:12 -0700

    wip

--- kyo-prelude/shared/src/main/scala/kyo/Parse.scala
+++ kyo-prelude/shared/src/main/scala/kyo/Parse.scala
@@ -4,12 +4,15 @@ import kyo.kernel.*
 
 case class AmbigousParse(seq: Seq[?]) extends Exception
 
-opaque type Parse <: (Var[String] & Choice) = Var[String] & Choice & Abort[AmbigousParse]
+opaque type Parse <: (Var[Text] & Choice) = Var[Text] & Choice & Abort[AmbigousParse]
 
 object Parse:
 
-    def read[A, S](f: String => Maybe[(String, A)] < S)(using Frame): A < (Parse & S) =
-        Var.use[String] { s =>
+    // avoids splicing the derived tag on each use
+    private given Tag[Var[Text]] = Tag[Var[Text]]
+
+    def read[A, S](f: Text => Maybe[(Text, A)] < S)(using Frame): A < (Parse & S) =
+        Var.use[Text] { s =>
             f(s).map {
                 case Present((remaining, result)) =>
                     Var.set(remaining).as(result)
@@ -25,7 +28,7 @@ object Parse:
     def oneOf[A, S](seq: (A < (Parse & S))*)(using Frame): A < (Parse & S) = Choice.eval(seq)(identity)
 
     def attempt[A: Flat, S](v: A < (Parse & S))(using Frame): Maybe[A] < (Parse & S) =
-        Var.use[String] { start =>
+        Var.use[Text] { start =>
             Choice.run(v).map { r =>
                 result(r).map {
                     case Absent =>
@@ -37,13 +40,13 @@ object Parse:
         }
 
     def peek[A: Flat, S](v: A < (Parse & S))(using Frame): Maybe[A] < (Parse & S) =
-        Var.use[String] { start =>
+        Var.use[Text] { start =>
             Choice.run(v).map { r =>
                 Var.set(start).as(result(r))
             }
         }
 
-    def run[A: Flat, S](input: String)(v: A < (Parse & S))(using Frame): Maybe[(String, A)] < (S & Abort[AmbigousParse]) =
+    def run[A: Flat, S](input: Text)(v: A < (Parse & S))(using Frame): Maybe[(Text, A)] < (S & Abort[AmbigousParse]) =
         Choice.run(Var.runTuple(input)(v)).map(result)
 
     def whitespaces(using Frame): Unit < Parse =
@@ -54,8 +57,8 @@ object Parse:
 
     def int(using Frame): Int < Parse =
         Parse.read { s =>
-            s.headOption match
-                case Some(c) if c == '-' || c.isDigit =>
+            s.head match
+                case Present(c) if c == '-' || c.isDigit =>
                     val (digits, rest) = s.tail.span(_.isDigit)
                     val numStr         = if c == '-' then "-" + digits else s"$c$digits"
                     try
@@ -68,18 +71,18 @@ object Parse:
 
     def decimal(using Frame): Double < Parse =
         Parse.read { s =>
-            def isValidDecimal(str: String) =
+            def isValidDecimal(str: Text) =
                 str.count(_ == '.') <= 1 &&
                     str.filterNot(c => c.isDigit || c == '.' || c == '-').isEmpty &&
-                    (str.length > 1 || str == "0") // Handles single-char edge cases
+                    (str.length > 1 || str.is("0")) // Handles single-char edge cases
 
-            s.headOption match
-                case Some(c) if c == '-' || c == '.' || c.isDigit =>
+            s.head match
+                case Present(c) if c == '-' || c == '.' || c.isDigit =>
                     // Capture the decimal part including optional '-' prefix, digits before/after '.'
                     val (numStr, rest) = s.span(c => c.isDigit || c == '.' || c == '-')
                     if isValidDecimal(numStr) then
                         try
-                            Maybe((rest, numStr.toDouble))
+                            Maybe((rest, numStr.toString.toDouble))
                         catch
                             case _: NumberFormatException => Maybe.empty
                     else Maybe.empty
@@ -89,60 +92,28 @@ object Parse:
             end match
         }
 
-    def string(using Frame): String < Parse =
-        Parse.read { s =>
-            s.headOption match
-                case Some('"') =>
-                    var escaped = false
-                    var i       = 1
-                    var builder = new StringBuilder
-                    while i < s.length && (escaped || s(i) != '"') do
-                        if escaped then
-                            builder.append(s(i) match
-                                case 'n' => '\n'
-                                case 't' => '\t'
-                                case 'r' => '\r'
-                                case c   => c
-                            )
-                            escaped = false
-                        else if s(i) == '\\' then
-                            escaped = true
-                        else
-                            builder.append(s(i))
-                        end if
-                        i += 1
-                    end while
-
-                    if i < s.length && s(i) == '"' then
-                        Maybe((s.substring(i + 1), builder.toString))
-                    else
-                        Maybe.empty
-                    end if
-                case _ => Maybe.empty
-        }
-
     def boolean(using Frame): Boolean < Parse =
         Parse.read { s =>
             if s.startsWith("true") then
-                Maybe((s.substring(4), true))
+                Maybe((s.drop(4), true))
             else if s.startsWith("false") then
-                Maybe((s.substring(5), false))
+                Maybe((s.drop(5), false))
             else
                 Maybe.empty
         }
 
     def char(c: Char)(using Frame): Char < Parse =
         Parse.read { s =>
-            if s.startsWith(c.toString) then
-                Maybe((s.substring(1), c))
+            if s.startsWith(s"$c") then
+                Maybe((s.drop(1), c))
             else
                 Maybe.empty
         }
 
-    def literal(str: String)(using Frame): String < Parse =
+    def literal(str: Text)(using Frame): Text < Parse =
         Parse.read { s =>
             if s.startsWith(str) then
-                Maybe((s.substring(str.length), str))
+                Maybe((s.drop(str.length), str))
             else
                 Maybe.empty
         }
