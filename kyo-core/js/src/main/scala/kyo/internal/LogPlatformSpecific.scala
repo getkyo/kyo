9fa34f8303363abbd2b9b86db1c2b35c554b3fcb
Flavio Brasil <fwbrasil@gmail.com>
2024-10-27 20:26:36 -0700

    fix js build

--- kyo-core/js/src/main/scala/kyo/internal/LogPlatformSpecific.scala
+++ kyo-core/js/src/main/scala/kyo/internal/LogPlatformSpecific.scala
@@ -3,4 +3,4 @@ package kyo.internal
 import kyo.Log
 
 trait LogPlatformSpecific:
-    val live: Log = Log(Log.Unsafe.ConsoleLogger("kyo.logs", Log.Level.Debug))
+    val live: Log = Log(Log.Unsafe.ConsoleLogger("kyo.logs", Log.Level.debug))
