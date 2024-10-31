8682e6cf574f4bbc0461863921920e7edeb3c684
Flavio Brasil <fwbrasil@gmail.com>
2024-10-31 13:18:35 -0700

    fix rate limit replenish with the new Clock api

--- kyo-core/shared/src/main/scala/kyo/Meter.scala
+++ kyo-core/shared/src/main/scala/kyo/Meter.scala
@@ -135,7 +135,7 @@ object Meter:
             new Base(rate, reentrant):
                 val timerTask =
                     // Schedule periodic task to replenish permits
-                    IO.Unsafe.run(Clock.repeatAtInterval(period)(replenish())).eval
+                    IO.Unsafe.run(Clock.repeatAtInterval(period, period)(replenish())).eval
 
                 def dispatch[A, S](v: => A < S) =
                     // Don't release a permit since it's managed by the timer task
