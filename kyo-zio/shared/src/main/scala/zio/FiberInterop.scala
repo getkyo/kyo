b209b7db3cee9dc9dfe808fd40b473b744fb9c7a
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2024-11-09 22:49:18 -0500

    [kyo-zio] use: `Fiber.Runtime.UnsafeAPI#interrupt` method (#833)

--- kyo-zio/shared/src/main/scala/zio/FiberInterop.scala
+++
@@ -1,5 +0,0 @@
-package zio
-
-extension [E, A](fiber: Fiber.Runtime[E, A])
-    def unsafeInterrupt()(using trace: Trace, u: Unsafe): Unit =
-        fiber.tellInterrupt(Cause.Interrupt(FiberId.None, StackTrace(FiberId.None, Chunk(trace))))
