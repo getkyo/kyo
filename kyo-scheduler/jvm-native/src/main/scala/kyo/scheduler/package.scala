2249aa640394d1290bd9f5a37b0e5c76fe769337
Ondra Pelech <ondra.pelech@gmail.com>
2024-11-27 00:12:57 +0100

    Use JUL for logging exceptions (bugs)

--- kyo-scheduler/jvm-native/src/main/scala/kyo/scheduler/package.scala
+++ kyo-scheduler/jvm-native/src/main/scala/kyo/scheduler/package.scala
@@ -1,13 +1,16 @@
 package kyo
 
+import java.util.logging.*
 import kyo.stats.internal.StatsRegistry
 
 package object scheduler {
+
+    private[scheduler] val log = Logger.getLogger("kyo.scheduler")
 
     private[scheduler] def statsScope =
         StatsRegistry.scope("kyo", "scheduler")
 
     private[scheduler] def bug(msg: String, ex: Throwable) =
-        new Exception(s"🙈 !!Kyo Scheduler Bug!! " + msg, ex).printStackTrace()
+        log.log(Level.SEVERE, s"🙈 !!Kyo Scheduler Bug!!", new Exception(msg, ex))
 
 }
