731933cc429378216b6d30099bf248ae05363115
Flavio Brasil <fwbrasil@gmail.com>
2024-12-01 10:58:02 -0800

    RefLog scaladoc

--- kyo-stm/shared/src/main/scala/kyo/RefLog.scala
+++ kyo-stm/shared/src/main/scala/kyo/RefLog.scala
@@ -2,6 +2,16 @@ package kyo
 
 import STM.internal.*
 
+/** A log of transactional operations performed on TRefs within an STM transaction.
+  *
+  * RefLog maintains a mapping from transactional references to their pending read/write operations within a transaction. It tracks both
+  * read entries (which record the version of data read) and write entries (which contain the new values to be committed).
+  *
+  * This type is used internally by the STM implementation and should not be accessed directly by application code.
+  *
+  * @note
+  *   This is a private implementation detail of the STM system
+  */
 opaque type RefLog = Map[TRef[Any], Entry[Any]]
 
 private[kyo] object RefLog:
