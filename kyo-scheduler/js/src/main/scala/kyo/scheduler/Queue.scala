3af1a44c03e7ad6a48766bf2ce05c98f6cc7bbf8
Eddy Oyieko <oyieko99@gmail.com>
2024-12-09 18:17:56 +0300

    added Queue to kyo-schedulerJS

---
+++ kyo-scheduler/js/src/main/scala/kyo/scheduler/Queue.scala
@@ -0,0 +1,35 @@
+package kyo.scheduler
+
+import java.lang.invoke.VarHandle
+import java.util.concurrent.atomic.AtomicBoolean
+import scala.collection.mutable.PriorityQueue
+
+class Queue[A](implicit ord: Ordering[A]) extends AtomicBoolean {
+    private val queue = PriorityQueue[A]()
+
+    private var items = 0
+
+    private def lock(): Unit =
+        while (!compareAndSet(false, true)) ()
+
+    private def unlock(): Unit = set(false)
+
+    def isEmpty() = size() == 0
+
+    def size(): Int = {
+        VarHandle.acquireFence()
+        items
+    }
+
+    def drainToSeq(): Seq[A] = {
+        if (!isEmpty()) {
+            lock()
+            try {
+                items = 0
+                queue.dequeueAll
+            } finally
+                unlock()
+        } else
+            Seq.empty
+    }
+}
