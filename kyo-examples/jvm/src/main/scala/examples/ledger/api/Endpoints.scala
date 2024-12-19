930d2596d539fc1c17ce0afc2eb96daabd87b178
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-12-19 10:00:30 -0600

    Attempting to add Swagger doc generation to the Endpoints type

--- kyo-examples/jvm/src/main/scala/examples/ledger/api/Endpoints.scala
+++ kyo-examples/jvm/src/main/scala/examples/ledger/api/Endpoints.scala
@@ -7,6 +7,7 @@ import sttp.model.StatusCode
 import sttp.tapir.*
 import sttp.tapir.generic.auto.*
 import sttp.tapir.json.zio.*
+import sttp.tapir.swagger.bundle.SwaggerInterpreter
 import zio.json.JsonEncoder
 
 object Endpoints:
@@ -15,32 +16,39 @@ object Endpoints:
 
         val handler = await(Env.get[Handler])
 
-        await {
-            Routes.add(
-                _.post
-                    .in("clientes" / path[Int]("id") / "transacoes")
-                    .errorOut(statusCode)
-                    .in(jsonBody[Transaction])
-                    .out(jsonBody[Processed])
-            )(handler.transaction)
-        }
+        val apiRoutes =
+            await {
+                Routes.add(
+                    _.post
+                        .in("clientes" / path[Int]("id") / "transacoes")
+                        .errorOut(statusCode)
+                        .in(jsonBody[Transaction])
+                        .out(jsonBody[Processed])
+                )(handler.transaction)
+            }
 
-        await {
-            Routes.add(
-                _.get
-                    .in("clientes" / path[Int]("id") / "extrato")
-                    .errorOut(statusCode)
-                    .out(jsonBody[Statement])
-            )(handler.statement)
-        }
+            await {
+                Routes.add(
+                    _.get
+                        .in("clientes" / path[Int]("id") / "extrato")
+                        .errorOut(statusCode)
+                        .out(jsonBody[Statement])
+                )(handler.statement)
+            }
 
-        await {
-            Routes.add(
-                _.get
-                    .in("health")
-                    .out(stringBody)
-            )(_ => "ok")
-        }
+            await {
+                Routes.add(
+                    _.get
+                        .in("health")
+                        .out(stringBody)
+                )(_ => "ok")
+            }
+        end apiRoutes
+
+        val endpoints  = await(Routes.get(apiRoutes).map(rs => rs.map(r => r.endpoint))).toList
+        val docsRoutes = await(Routes.from(SwaggerInterpreter().fromServerEndpoints(endpoints, "Ledger API", "1.0")))
+        val allRoutes  = await(Routes.collect(apiRoutes, docsRoutes))
+        allRoutes
     }
 
 end Endpoints
