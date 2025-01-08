cbffd29ccf34fc463c7f94e9345ddb4a6b4d61c8
Flavio Brasil <fwbrasil@gmail.com>
2025-01-08 15:17:21 -0800

    [direct] improve error messages + test coverage

--- kyo-direct/shared/src/test/scala/kyo/Test.scala
+++ kyo-direct/shared/src/test/scala/kyo/Test.scala
@@ -3,15 +3,26 @@ package kyo
 import kyo.internal.BaseKyoTest
 import kyo.kernel.Platform
 import org.scalatest.NonImplicitAssertions
+import org.scalatest.Tag
 import org.scalatest.freespec.AsyncFreeSpec
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
-abstract class Test extends AsyncFreeSpec with BaseKyoTest[IO] with NonImplicitAssertions:
+abstract class Test extends AsyncFreeSpec with BaseKyoTest[Abort[Any] & Async & Resource] with NonImplicitAssertions:
 
-    def run(v: Future[Assertion] < IO): Future[Assertion] =
+    def run(v: Future[Assertion] < (Abort[Any] & Async & Resource)): Future[Assertion] =
         import AllowUnsafe.embrace.danger
-        IO.Unsafe.evalOrThrow(v)
+        v.pipe(
+            Resource.run,
+            Abort.recover[Any] {
+                case ex: Throwable => throw ex
+                case e             => throw new IllegalStateException(s"Test aborted with $e")
+            },
+            Async.run,
+            _.map(_.toFuture).map(_.flatten),
+            IO.Unsafe.evalOrThrow
+        )
+    end run
 
     type Assertion = org.scalatest.compatible.Assertion
     def success = succeed
