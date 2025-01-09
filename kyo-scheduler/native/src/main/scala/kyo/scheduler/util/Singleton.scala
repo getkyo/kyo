96b052c5223641e426f26e7c43b308e7a3e33677
Flavio Brasil <fwbrasil@gmail.com>
2025-01-09 08:13:22 -0800

    fix native build

--- kyo-scheduler/native/src/main/scala/kyo/scheduler/util/Singleton.scala
+++ kyo-scheduler/native/src/main/scala/kyo/scheduler/util/Singleton.scala
@@ -1,6 +1,8 @@
 package kyo.scheduler.util
 
-abstract class Singleton[A <: AnyRef](init: () => A) {
+abstract class Singleton[A <: AnyRef] {
+
+    protected def init(): A
 
     lazy val get = init()
 }
