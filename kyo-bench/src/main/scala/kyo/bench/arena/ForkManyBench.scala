999807f4bd907a36f35104f275ad90a0ea143c50
Flavio Brasil <fwbrasil@users.noreply.github.com>
2025-01-29 11:10:22 -0800

    [bench] separate comparative benchmarks (#1060)
    <!--
    PRs require an approval from any of the core contributors, other than
    the PR author.
    
    Include this header if applicable:
    Fixes #issue1, #issue2, ...
    -->
    
    ### Problem
    <!--
    Explain here the context, and why you're making this change. What is the
    problem you're trying to solve?
    -->
    Kyo's benchmarks are currently all in the same module and package. As we
    evolve the library, we'll likely need to introduce benchmarks that
    aren't meant for comparing different effect systems. Having all
    benchmarks in the same package is also inconvenient when you want to
    execute only the comparative ones for example.
    
    ### Solution
    <!--
    Describe your solution. Focus on helping reviewers understand your
    technical approach and implementation decisions.
    -->
    Move comparative benchmarks to a separate `kyo.bench.arena` package.
    Benchmarks that don't follow the comparative pattern with sync/fork
    benchmarks were kept in the `kyo.bench` package.

---
+++ kyo-bench/src/main/scala/kyo/bench/arena/ForkManyBench.scala
@@ -0,0 +1,69 @@
+package kyo.bench.arena
+
+import org.openjdk.jmh.annotations.*
+
+class ForkManyBench extends ArenaBench.ForkOnly(0):
+
+    val depth = 10000
+
+    def catsBench() =
+        import cats.effect.IO
+
+        def repeat[A](n: Int)(io: IO[A]): IO[A] =
+            if n <= 1 then io
+            else io.flatMap(_ => repeat(n - 1)(io))
+
+        for
+            deferred <- IO.deferred[Unit]
+            ref      <- IO.ref(depth)
+            effect = ref
+                .modify(n => (n - 1, if n == 1 then deferred.complete(()) else IO.unit))
+                .flatten
+            _ <- repeat(depth)(effect.start)
+            _ <- deferred.get
+        yield 0
+        end for
+    end catsBench
+
+    override def kyoBenchFiber() =
+        import kyo.*
+
+        def repeat[A](n: Int)(io: A < IO): A < IO =
+            if n <= 1 then io
+            else io.flatMap(_ => repeat(n - 1)(io))
+
+        for
+            promise <- Promise.init[Nothing, Unit]
+            ref     <- AtomicInt.init(depth)
+            effect = ref.decrementAndGet.flatMap {
+                case 1 =>
+                    promise.complete(Result.unit)
+                case _ =>
+                    false
+            }
+            _ <- repeat(depth)(Async.run(effect))
+            _ <- promise.get
+        yield 0
+        end for
+    end kyoBenchFiber
+
+    def zioBench() =
+        import zio.{Promise, Ref, ZIO}
+
+        def repeat[R, E, A](n: Int)(zio: ZIO[R, E, A]): ZIO[R, E, A] =
+            if n <= 1 then zio
+            else zio *> repeat(n - 1)(zio)
+
+        for
+            promise <- Promise.make[Nothing, Unit]
+            ref     <- Ref.make(depth)
+            effect = ref
+                .modify(n => (if n == 1 then promise.succeed(()) else ZIO.unit, n - 1))
+                .flatten
+            _ <- repeat(depth)(effect.forkDaemon)
+            _ <- promise.await
+        yield 0
+        end for
+    end zioBench
+
+end ForkManyBench
