1f52ae7a332088d7827e96acdeb7cfc55b29e575
Akhilender Bongirwar <akhilenderb9@gmail.com>
2025-02-18 18:25:30 +0530

    fix: add suggestions
    Signed-off-by: Akhilender Bongirwar <akhilenderb9@gmail.com>

--- kyo-offheap/native/src/main/java/lang/foreign/MemorySegment.scala
+++ kyo-offheap/native/src/main/java/lang/foreign/MemorySegment.scala
@@ -26,29 +26,47 @@ final class MemorySegment private (ptr: Ptr[Byte], val byteSize: Long) {
 
   /** Reads a value from memory using the provided layout.
     */
+  def get(layout: ValueLayout.ofBoolean, offset: Long): Boolean = {
+    require(offset + unsafe.sizeOf[Bool] <= byteSize)
+    !(ptr + offset).cast[Bool]
+  }
   def get(layout: ValueLayout.ofByte, offset: Long): Byte = {
+    require(offset + unsafe.sizeOf[Byte] <= byteSize)
     !(ptr + offset).cast[Byte]
   }
   def get(layout: ValueLayout.ofShort, offset: Long): Short = {
+    require(offset + unsafe.sizeOf[Short] <= byteSize)
     !(ptr + offset).cast[Short]
   }
   def get(layout: ValueLayout.ofInt, offset: Long): Int = {
+    require(offset + unsafe.sizeOf[Int] <= byteSize)
     !(ptr + offset).cast[Int]
   }
   def get(layout: ValueLayout.ofLong, offset: Long): Long = {
+    require(offset + unsafe.sizeOf[Long] <= byteSize)
     !(ptr + offset).cast[Long]
   }
   def get(layout: ValueLayout.ofFloat, offset: Long): Float = {
+    require(offset + unsafe.sizeOf[Float] <= byteSize)
     !(ptr + offset).cast[Float]
   }
   def get(layout: ValueLayout.ofDouble, offset: Long): Double = {
+    require(offset + unsafe.sizeOf[Double] <= byteSize)
     !(ptr + offset).cast[Double]
   }
-  def get(layout: ValueLayout, offset: Long): MemorySegment =
+  def get(layout: ValueLayout.ofChar, offset: Long): Char = {
+    require(offset + unsafe.sizeOf[Char] <= byteSize)
+    !(ptr + offset).cast[Char]
+  }
+  def get(layout: AddressLayout, offset: Long): MemorySegment =
     new MemorySegment(!(ptr + offset).cast[Ptr[Byte]], byteSize)
 
   /** Writes a value to memory using the provided layout.
     */
+  def set(layout: ValueLayout.ofBoolean, offset: Long, value: Boolean): Unit = {
+    require(offset + unsafe.sizeOf[Bool] <= byteSize)
+    !(ptr + offset).cast[Bool] = value
+  }
   def set(layout: ValueLayout.ofByte, offset: Long, value: Byte): Unit = {
     require(offset + unsafe.sizeOf[Byte] <= byteSize)
     !(ptr + offset).cast[Byte] = value
@@ -73,6 +91,12 @@ final class MemorySegment private (ptr: Ptr[Byte], val byteSize: Long) {
     require(offset + unsafe.sizeOf[Double] <= byteSize)
     !(ptr + offset).cast[Double] = value
   }
+  def set(layout: ValueLayout.ofChar, offset: Long, value: Char): Unit = {
+    require(offset + unsafe.sizeOf[Char] <= byteSize)
+    !(ptr + offset).cast[Char] = value
+  }
+  def set(layout: AddressLayout, offset: Long, value: MemorySegment): Unit =
+    !(ptr + offset).cast[Ptr[Byte]] = value.ptr
 }
 
 object MemorySegment {
@@ -101,17 +125,28 @@ object MemorySegment {
 sealed trait ValueLayout
 
 object ValueLayout {
-  case object JAVA_BYTE extends ValueLayout
-  case object JAVA_SHORT extends ValueLayout
-  case object JAVA_INT extends ValueLayout
-  case object JAVA_LONG extends ValueLayout
-  case object JAVA_FLOAT extends ValueLayout
-  case object JAVA_DOUBLE extends ValueLayout
+  sealed trait ofBoolean extends ValueLayout
+  sealed trait ofByte extends ValueLayout
+  sealed trait ofChar extends ValueLayout
+  sealed trait ofShort extends ValueLayout
+  sealed trait ofInt extends ValueLayout
+  sealed trait ofLong extends ValueLayout
+  sealed trait ofFloat extends ValueLayout
+  sealed trait ofDouble extends ValueLayout
 
-  type ofByte = JAVA_BYTE.type
-  type ofShort = JAVA_SHORT.type
-  type ofInt = JAVA_INT.type
-  type ofLong = JAVA_LONG.type
-  type ofFloat = JAVA_FLOAT.type
-  type ofDouble = JAVA_DOUBLE.type
+  case object JAVA_BOOLEAN extends ValueLayout
+  case object JAVA_BYTE extends ValueLayout
+  case object JAVA_CHAR extends ValueLayout
+  case object JAVA_CHAR_UNALIGNED extends ValueLayout
+  case object JAVA_SHORT extends ValueLayout
+  case object JAVA_SHORT_UNALIGNED extends ValueLayout
+  case object JAVA_INT extends ValueLayout
+  case object JAVA_INT_UNALIGNED extends ValueLayout
+  case object JAVA_LONG extends ValueLayout
+  case object JAVA_LONG_UNALIGNED extends ValueLayout
+  case object JAVA_FLOAT extends ValueLayout
+  case object JAVA_FLOAT_UNALIGNED extends ValueLayout
+  case object JAVA_DOUBLE extends ValueLayout
+  case object JAVA_DOUBLE_UNALIGNED extends ValueLayout
 }
+sealed trait AddressLayout extends ValueLayout
