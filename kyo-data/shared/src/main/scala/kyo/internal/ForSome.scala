d9b3b68e3465b6fa92c5c3716d6b90cc7a84d1bc
Aleksey Troitskiy <amtroitskiy@gmail.com>
2025-03-18 12:38:26 +0300

    encode ForSome without instanceOf

--- kyo-data/shared/src/main/scala/kyo/internal/ForSome.scala
+++ kyo-data/shared/src/main/scala/kyo/internal/ForSome.scala
@@ -4,49 +4,38 @@ package kyo.internal
   */
 type ForSome[F[_]] = ForSome.Type[F]
 object ForSome:
-    type Type[F[_]] <: { type A }
+    class Unwrap[F[_], A](val unwrap: F[A]) extends AnyVal
+
+    opaque type Type[F[_]] <: Unwrap[F, ?] = Unwrap[F, ?]
 
     /** Converts value of type `F[A]` to existential form
       */
-    inline def apply[F[_], A](f: F[A]): Type[F] = f.asInstanceOf[Type[F]]
-
-    extension [F[_]](value: Type[F])
-        /** Converts value of `ForSome[F]` to `F[value.A]`
-          */
-        inline def unwrap: F[value.A] = value.asInstanceOf[F[value.A]]
-    end extension
+    inline def apply[F[_], A](v: F[A]): Type[F] = Unwrap(v)
 
     inline def of[F[_]]: OfOps[F] = new OfOps[F](())
     class OfOps[F[_]](dummy: Unit) extends AnyVal:
-        /** Converts value of type `F[A]` to existential forms
+        /** Converts value of type `F[A]` to existential form
           */
         inline def apply[A](f: F[A]): Type[F] = ForSome(f)
     end OfOps
 end ForSome
 
-/** Existentinal types encoding for type constructors `F[_, _]`. `ForSome2[F]` is semantically equiavalent to `F[?, ?]`.
+/** Existentinal types encoding for type constructors `F[_, _]`. `ForSome2[F]` is semantically equiavalent to `F[?, ?]`
   */
 type ForSome2[F[_, _]] = ForSome2.Type[F]
 object ForSome2:
-    type Type[F[_, _]] <: { type A1; type A2 }
+    class Unwrap[F[_, _], A1, A2](val unwrap: F[A1, A2]) extends AnyVal
 
-    /** Converts value of type `F[A1, A2]` to existential form
+    opaque type Type[F[_, _]] <: Unwrap[F, ?, ?] = Unwrap[F, ?, ?]
+
+    /** Converts value of type `F[A]` to existential form
       */
-    inline def apply[F[_, _], A1, A2](f: F[A1, A2]): Type[F] = f.asInstanceOf[Type[F]]
-
-    inline def unapply[F[_, _]](value: Type[F]): F[value.A1, value.A2] = value.asInstanceOf[F[value.A1, value.A2]]
-
-    extension [F[_, _]](value: Type[F])
-        /** Converts value of `ForSome2[F]` to `F[value.A1, value.A2]`
-          */
-        inline def unwrap: F[value.A1, value.A2] = unapply(value)
-    end extension
+    inline def apply[F[_, _], A1, A2](v: F[A1, A2]): Type[F] = Unwrap(v)
 
     inline def of[F[_, _]]: OfOps[F] = new OfOps[F](())
-
     class OfOps[F[_, _]](dummy: Unit) extends AnyVal:
         /** Converts value of type `F[A1, A2]` to existential form
           */
-        def apply[A1, A2](f: F[A1, A2]): Type[F] = ForSome2(f)
+        inline def apply[A1, A2](f: F[A1, A2]): Type[F] = ForSome2(f)
     end OfOps
 end ForSome2
