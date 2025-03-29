744f81fcbec3d73e3c07468a9b9abcc2c249a221
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2025-03-29 14:16:09 -0700

    [prelude] Abort with literal types (#1118)
    Fixes #739 
    
    ### Problem
    With `Abort.fail`, manual type annotations are required to write
    expressions that fail with literal types.
    
    ### Solution
    Add an object `literal` to `Abort` to enable failing with literal types,
    namely Strings.
    
    ### Notes
    The functions will degrade to non-literal behavior in a generic scope.

--- kyo-prelude/shared/src/test/scala/kyo/AbortTest.scala
+++ kyo-prelude/shared/src/test/scala/kyo/AbortTest.scala
@@ -1214,4 +1214,77 @@ class AbortTest extends Test:
         }
     }
 
+    "literal" - {
+        "string" in {
+            val result                      = Abort.literal.fail("FAIL!")
+            val _: Nothing < Abort["FAIL!"] = result
+            val _: Nothing < Abort[String]  = result
+            assert(Abort.run(result).eval == Result.fail("FAIL!"))
+        }
+
+        "numeric" in {
+            val result                 = Abort.literal.fail(-1)
+            val _: Nothing < Abort[-1] = result
+            assert(Abort.run(result).eval == Result.fail(-1))
+        }
+
+        "ensuring" in {
+            def divide(a: Int, b: Int): Int < Abort["Division by zero"] =
+                Abort.literal.ensuring(b != 0, a / b)("Division by zero")
+
+            val result                             = divide(1, 0)
+            val _: Int < Abort["Division by zero"] = result
+            assert(Abort.run(result).eval == Result.fail("Division by zero"))
+            assert(Abort.run(divide(1, 1)).eval == Result.succeed(1))
+        }
+
+        "unless" in {
+            def unless(b: Boolean): Unit < Abort["BOOM"] =
+                Abort.literal.unless(b)("BOOM")
+
+            val result                  = unless(false)
+            val _: Unit < Abort["BOOM"] = result
+            assert(Abort.run(result).eval == Result.fail("BOOM"))
+            assert(Abort.run(unless(true)).eval == Result.unit)
+        }
+
+        "when" in {
+            def when(b: Boolean): Unit < Abort["TOO_BIG"] =
+                Abort.literal.when(b)("TOO_BIG")
+
+            val result                     = when(false)
+            val _: Unit < Abort["TOO_BIG"] = result
+            assert(Abort.run(result).eval == Result.unit)
+            assert(Abort.run(when(true)).eval == Result.fail("TOO_BIG"))
+        }
+
+        "effects" in {
+            val result = Env.run(15) {
+                Abort.literal.run {
+                    for
+                        x <- Env.get[Int]
+                        _ <- Abort.literal.when(x > 10)("TOO_BIG")
+                    yield x
+                }
+            }.eval
+            val _: Result["TOO_BIG", Int] = result
+            assert(result == Result.fail("TOO_BIG"))
+        }
+
+        "generic" in {
+            def test[A](a: A): Nothing < Abort[A] =
+                Abort.literal.fail(a)
+
+            val result                  = test(1)
+            val _: Nothing < Abort[Int] = result
+            assert(Abort.run(result).eval == Result.fail(1))
+        }
+
+        "union" in {
+            val result                                 = Abort.literal.fail("FAIL!")
+            val _: Nothing < Abort["FAIL!" | "FAIL2!"] = result
+            assert(Abort.run(result).eval == Result.fail("FAIL!"))
+        }
+    }
+
 end AbortTest
