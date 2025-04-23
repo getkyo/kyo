0d086767bcd2d3a34b02905055773a4500f767b4
John Hungerford <hungerfordjustice@gmail.com>
2025-04-23 09:59:12 -0400

    Stubbed out remaining async map methods

--- kyo-core/shared/src/main/scala/kyo/StreamCoreExtensions.scala
+++ kyo-core/shared/src/main/scala/kyo/StreamCoreExtensions.scala
@@ -182,12 +182,14 @@ object StreamCoreExtensions:
         ): Stream[V, Abort[E] & S & Async] =
             other.mergeHaltingLeft(stream)(using i1, i2, sct, t, f)
 
-        /** Applies effectful transformation of stream asynchronously, mapping elements in parallel. Preserves chunk boundaries.
+        /** Applies effectful transformation of stream elements asynchronously, mapping them in parallel. Preserves chunk boundaries.
           *
           * @param parallel
           *   Maximum number of elements to transform in parallel at a time
           * @param bufferSize
           *   Size of buffer used to mediate
+          * @param f
+          *   Asynchronous transformation of stream elements
           */
         def mapPar[V2, S2](parallel: Int, bufferSize: Int = defaultAsyncStreamBufferSize)(f: V => V2 < (Abort[E] & Async & S2))(
             using
@@ -268,6 +270,11 @@ object StreamCoreExtensions:
                 }
         end mapPar
 
+        /** Applies effectful transformation of stream elements asynchronously, mapping them in parallel. Preserves chunk boundaries.
+          *
+          * @param f
+          *   Asynchronous transformation of stream elements
+          */
         def mapPar[V2, S2](f: V => V2 < (Abort[E] & Async & S2))(
             using
             t1: Tag[Emit[Chunk[V]]],
@@ -278,6 +285,186 @@ object StreamCoreExtensions:
             frame: Frame
         ): Stream[V2, Abort[E] & Async & S & S2] =
             mapPar(Async.defaultConcurrency, defaultAsyncStreamBufferSize)(f)(using t1, t2, t3, i1, i2, frame)
+
+        /** Applies effectful transformation of stream elements asynchronously, mapping them in parallel. Preserves chunk boundaries.
+          *
+          * @param parallel
+          *   Maximum number of elements to transform in parallel at a time
+          * @param bufferSize
+          *   Size of buffer used to mediate
+          * @param f
+          *   Asynchronous transformation of stream elements
+          */
+        def mapParUnordered[V2, S2](parallel: Int, bufferSize: Int = defaultAsyncStreamBufferSize)(f: V => V2 < (Abort[E] & Async & S2))(
+            using
+            t1: Tag[Emit[Chunk[V]]],
+            t2: Tag[Emit[Chunk[V2]]],
+            t3: Tag[V2],
+            i1: Isolate.Contextual[S & S2, IO],
+            i2: Isolate.Stateful[S & S2, Abort[E] & Async],
+            frame: Frame
+        ): Stream[V2, Abort[E] & Async & S & S2] =
+            val initialState: (Fiber[E | Closed, Unit], Int) = (Fiber.unit, parallel)
+            Stream[V2, S & S2 & Abort[E] & Async]:
+                Channel.init[Maybe[Chunk[V2]]](bufferSize).map { channel =>
+                    AtomicInt.init(0).map: parAdjustmentRef =>
+                        val background = Async.run:
+                            val handledStream = ArrowEffect.handleState(t1, initialState, stream.emit)(
+                                handle = [C] =>
+                                    (input, state, cont) =>
+                                        val (prevEmitFiber, remainingEmitPar) = state
+
+                                        Loop(Fiber.success[E | Closed, Chunk[V2]](Chunk.empty[V2]), input, remainingEmitPar):
+                                            (prevChunkFiber, remainingChunk, remainingPar) =>
+                                                parAdjustmentRef.getAndSet(0).map: parAdjustment =>
+                                                    val adjustedRemainingPar = remainingPar + parAdjustment
+                                                    val nextParSection       = remainingChunk.take(adjustedRemainingPar)
+
+                                                    val nextChunkEffect = Async.foreach(nextParSection)(f)
+
+                                                    val newRemainingPar   = adjustedRemainingPar - nextParSection.size
+                                                    val newRemainingChunk = remainingChunk.drop(adjustedRemainingPar)
+
+                                                    if newRemainingPar <= 0 && newRemainingChunk.size <= 0 then
+                                                        nextChunkEffect.map: nextChunk =>
+                                                            prevChunkFiber.get.map: prevChunk =>
+                                                                prevEmitFiber.get.andThen:
+                                                                    channel.put(Present(prevChunk ++ nextChunk)).andThen:
+                                                                        Loop.done(((Fiber.unit, parallel), cont(())))
+                                                    else if newRemainingPar <= 0 then
+                                                        nextChunkEffect.map: nextChunk =>
+                                                            prevChunkFiber.get.map: prevChunk =>
+                                                                prevEmitFiber.get.andThen:
+                                                                    Loop.continue(
+                                                                        Fiber.success(prevChunk ++ nextChunk),
+                                                                        newRemainingChunk,
+                                                                        parallel
+                                                                    )
+                                                    else if newRemainingChunk.size <= 0 then
+                                                        Async.run {
+                                                            nextChunkEffect.map: nextChunk =>
+                                                                prevChunkFiber.get.map: prevChunk =>
+                                                                    prevEmitFiber.get.andThen:
+                                                                        val chunk = prevChunk ++ nextChunk
+                                                                        channel.put(Present(chunk)).andThen:
+                                                                            parAdjustmentRef.updateAndGet(_ + chunk.size).unit
+                                                        }.map: nextFiber =>
+                                                            Loop.done(((nextFiber, newRemainingPar), cont(())))
+                                                    else
+                                                        Async.run {
+                                                            nextChunkEffect.map: nextChunk =>
+                                                                prevChunkFiber.get.map: prevChunk =>
+                                                                    prevChunk ++ nextChunk
+                                                        }.map: nextFiber =>
+                                                            Loop.continue(nextFiber, newRemainingChunk, newRemainingPar)
+                                                    end if
+                                ,
+                                done = {
+                                    case ((lastFiber, _), _) =>
+                                        lastFiber.get.andThen:
+                                            channel.put(Absent)
+                                }
+                            )
+
+                            Abort.recover[Closed](_ => bug("Unexpected channel closure"))(handledStream)
+
+                        background.map: backgroundFiber =>
+                            emitMaybeChunksFromChannel(channel).andThen:
+                                backgroundFiber.get.unit
+                }
+        end mapParUnordered
+
+        /** Applies effectful transformation of stream elements asynchronously, mapping them in parallel. Preserves chunk boundaries.
+          *
+          * @param f
+          *   Asynchronous transformation of stream elements
+          */
+        def mapParUnordered[V2, S2](f: V => V2 < (Abort[E] & Async & S2))(
+            using
+            t1: Tag[Emit[Chunk[V]]],
+            t2: Tag[Emit[Chunk[V2]]],
+            t3: Tag[V2],
+            i1: Isolate.Contextual[S & S2, IO],
+            i2: Isolate.Stateful[S & S2, Abort[E] & Async],
+            frame: Frame
+        ): Stream[V2, Abort[E] & Async & S & S2] =
+            mapParUnordered(Async.defaultConcurrency, defaultAsyncStreamBufferSize)(f)(using t1, t2, t3, i1, i2, frame)
+
+        /** Applies effectful transformation of stream chunks asynchronously, mapping chunks in parallel. Preserves chunk boundaries.
+          *
+          * @param parallel
+          *   Maximum number of elements to transform in parallel at a time
+          * @param bufferSize
+          *   Size of buffer used to mediate
+          * @param f
+          *   Asynchronous transformation of stream elements
+          */
+        def mapChunkPar[V2, S2](
+            parallel: Int,
+            bufferSize: Int = defaultAsyncStreamBufferSize
+        )(f: Chunk[V] => Chunk[V2] < (Abort[E] & Async & S2))(
+            using
+            t1: Tag[Emit[Chunk[V]]],
+            t2: Tag[Emit[Chunk[V2]]],
+            t3: Tag[V2],
+            i1: Isolate.Contextual[S & S2, IO],
+            i2: Isolate.Stateful[S & S2, Abort[E] & Async],
+            frame: Frame
+        ): Stream[V2, Abort[E] & Async & S & S2] = ???
+
+        /** Applies effectful transformation of stream chunks asynchronously, mapping chunk in parallel. Preserves chunk boundaries.
+          *
+          * @param f
+          *   Asynchronous transformation of stream elements
+          */
+        def mapChunkPar[V2, S2](f: Chunk[V] => Chunk[V2] < (Abort[E] & Async & S2))(
+            using
+            t1: Tag[Emit[Chunk[V]]],
+            t2: Tag[Emit[Chunk[V2]]],
+            t3: Tag[V2],
+            i1: Isolate.Contextual[S & S2, IO],
+            i2: Isolate.Stateful[S & S2, Abort[E] & Async],
+            frame: Frame
+        ): Stream[V2, Abort[E] & Async & S & S2] =
+            mapChunkPar(Async.defaultConcurrency, defaultAsyncStreamBufferSize)(f)(using t1, t2, t3, i1, i2, frame)
+
+        /** Applies effectful transformation of stream chunks asynchronously, mapping chunks in parallel. Preserves chunk boundaries.
+          *
+          * @param parallel
+          *   Maximum number of elements to transform in parallel at a time
+          * @param bufferSize
+          *   Size of buffer used to mediate
+          * @param f
+          *   Asynchronous transformation of stream elements
+          */
+        def mapChunkParUnordered[V2, S2](
+            parallel: Int,
+            bufferSize: Int = defaultAsyncStreamBufferSize
+        )(f: Chunk[V] => Chunk[V2] < (Abort[E] & Async & S2))(
+            using
+            t1: Tag[Emit[Chunk[V]]],
+            t2: Tag[Emit[Chunk[V2]]],
+            t3: Tag[V2],
+            i1: Isolate.Contextual[S & S2, IO],
+            i2: Isolate.Stateful[S & S2, Abort[E] & Async],
+            frame: Frame
+        ): Stream[V2, Abort[E] & Async & S & S2] = ???
+
+        /** Applies effectful transformation of stream chunks asynchronously, mapping chunk in parallel. Preserves chunk boundaries.
+          *
+          * @param f
+          *   Asynchronous transformation of stream elements
+          */
+        def mapChunkParUnordered[V2, S2](f: Chunk[V] => Chunk[V2] < (Abort[E] & Async & S2))(
+            using
+            t1: Tag[Emit[Chunk[V]]],
+            t2: Tag[Emit[Chunk[V2]]],
+            t3: Tag[V2],
+            i1: Isolate.Contextual[S & S2, IO],
+            i2: Isolate.Stateful[S & S2, Abort[E] & Async],
+            frame: Frame
+        ): Stream[V2, Abort[E] & Async & S & S2] =
+            mapChunkParUnordered(Async.defaultConcurrency, defaultAsyncStreamBufferSize)(f)(using t1, t2, t3, i1, i2, frame)
 
     end extension
 end StreamCoreExtensions
