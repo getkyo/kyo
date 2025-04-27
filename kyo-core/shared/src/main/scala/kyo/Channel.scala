26a379a01b25a0e620d383123096d96406538465
Flavio Brasil <fwbrasil@gmail.com>
2025-04-27 16:15:24 -0700

    fix merge issue

--- kyo-core/shared/src/main/scala/kyo/Channel.scala
+++ kyo-core/shared/src/main/scala/kyo/Channel.scala
@@ -248,7 +248,7 @@ object Channel:
                 Loop[Unit, Unit, Abort[Closed] & Async & Emit[Chunk[A]]](()): _ =>
                     Channel.take(self).map: a =>
                         drainEffect.map: chunk =>
-                            Emit.valueWith(Chunk.single(a).concat(chunk))(Loop.continue)
+                            Emit.valueWith(Chunk(a).concat(chunk))(Loop.continue)
 
         /** Stream elements from channel, optionally specifying a maximum chunk size. In the absence of [[maxChunkSize]], chunk sizes will
           * be limited only by channel capacity or the number of elements in the channel at a given time. (Chunks can still be larger than
