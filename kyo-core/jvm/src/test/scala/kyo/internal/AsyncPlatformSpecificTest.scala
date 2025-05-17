3e17a798598b10e13abbdac745ff0e9a821d355e
Flavio Brasil <fwbrasil@gmail.com>
2025-05-17 01:31:06 -0700

    [core] move fromCompletionStage from Fiber to Async

--- kyo-core/jvm/src/test/scala/kyo/internal/AsyncPlatformSpecificTest.scala
+++ kyo-core/jvm/src/test/scala/kyo/internal/AsyncPlatformSpecificTest.scala
@@ -3,12 +3,12 @@ package kyo.internal
 import java.util.concurrent.*
 import kyo.*
 
-class FiberPlatformSpecificTest extends Test:
+class AsyncPlatformSpecificTest extends Test:
 
     "completionStage to Fiber" in runJVM {
         val cf = new CompletableFuture[Int]()
         cf.completeOnTimeout(42, 1, TimeUnit.MICROSECONDS)
-        val res = Fiber.fromCompletionStage(cf)
+        val res = Async.fromCompletionStage(cf)
         res.map(v => assert(v == 42))
     }
-end FiberPlatformSpecificTest
+end AsyncPlatformSpecificTest
