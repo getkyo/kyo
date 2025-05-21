a752eb57637db85703082bbbc350d0b8439cdab9
Flavio Brasil <fwbrasil@users.noreply.github.com>
2025-05-21 02:56:29 -0300

    [core] move fromCompletionStage from Fiber to Async (#1195)
    ### Problem
    
    `Fiber` is a low-level effect with complex semantics for newcomers. Its
    functionality is essential for integrations and more low-level code but
    `Async` provides better usability and covers a majority of use cases.
    For this reason, `Fiber`'s API has become more limited over time and we
    don't even provide common combinators like `race` for it since the same
    functionality can be achieved via `Async` combinators and `Async.run` to
    obtain a `Fiber`. I hadn't noticed that `fromCompletionStage` is still
    in `Fiber` and using an old method naming pattern with a `Fiber` suffix.
    
    ### Solution
    
    Move the method to `Async` and remove `fromCompletionStageFiber`.

--- kyo-core/jvm/src/main/scala/kyo/internal/AsyncPlatformSpecific.scala
+++ kyo-core/jvm/src/main/scala/kyo/internal/AsyncPlatformSpecific.scala
@@ -3,17 +3,18 @@ package kyo.internal
 import java.util.concurrent.CompletionStage
 import kyo.*
 
-trait FiberPlatformSpecific:
-    def fromCompletionStage[A](cs: CompletionStage[A])(using Frame): A < Async =
-        fromCompletionStageFiber(cs).map(_.get)
+trait AsyncPlatformSpecific:
 
-    def fromCompletionStageFiber[A](cs: CompletionStage[A])(using Frame): Fiber[Nothing, A] < IO =
+    def fromFuture[A](cs: CompletionStage[A])(using Frame): A < Async =
+        fromCompletionStage(cs)
+
+    def fromCompletionStage[A](cs: CompletionStage[A])(using Frame): A < Async =
         IO.Unsafe {
             val p = Promise.Unsafe.init[Nothing, A]()
             cs.whenComplete { (success, error) =>
                 if error == null then p.completeDiscard(Result.succeed(success))
                 else p.completeDiscard(Result.panic(error))
             }
-            p.safe
+            p.safe.get
         }
-end FiberPlatformSpecific
+end AsyncPlatformSpecific
