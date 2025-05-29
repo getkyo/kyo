b2cf1c4c09905a6a3e60f817113c91d70d2bfae1
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-05-30 00:33:24 +0200

    [direct] fix kyo-kernel test

--- kyo-kernel/shared/src/test/scala/kyo/KyoTest.scala
+++ kyo-kernel/shared/src/test/scala/kyo/KyoTest.scala
@@ -286,12 +286,11 @@ class KyoTest extends Test:
         }
 
         "shiftedWhile" in {
-
             def isEven(i: Int): Boolean < Any = i % 2 == 0
 
             def lessThanThree(i: Int): Boolean < Any = i < 3
 
-            val countWhile = shiftedWhile(Chunk(1, 2, 3, 4, 5))(
+            val countWhile = Kyo.shiftedWhile(Chunk(1, 2, 3, 4, 5))(
                 prolog = 0,
                 f = lessThanThree,
                 acc = (count, include, _) => count + 1,
@@ -299,7 +298,7 @@ class KyoTest extends Test:
             )
             assert(countWhile.eval == 3)
 
-            val sumWithMessage = shiftedWhile(Chunk(1, 2, -1, 3, 4))(
+            val sumWithMessage = Kyo.shiftedWhile(Chunk(1, 2, -1, 3, 4))(
                 prolog = 0,
                 f = lessThanThree,
                 acc = (sum, include, curr) => if include then sum + curr else sum,
@@ -307,7 +306,7 @@ class KyoTest extends Test:
             )
             assert(sumWithMessage.eval == "Sum: 2") // 1 + 2 + (-1) = 2
 
-            val collectUntilOdd = shiftedWhile(Chunk(2, 4, 6, 7, 8))(
+            val collectUntilOdd = Kyo.shiftedWhile(Chunk(2, 4, 6, 7, 8))(
                 prolog = List.empty,
                 f = isEven,
                 acc = (list, include, curr) => if include then curr :: list else list,
@@ -315,7 +314,7 @@ class KyoTest extends Test:
             )
             assert(collectUntilOdd.eval == List(2, 4, 6))
 
-            val emptyTest = shiftedWhile(Seq.empty[Int])(
+            val emptyTest = Kyo.shiftedWhile(Seq.empty[Int])(
                 prolog = "Default",
                 f = _ => true,
                 acc = (str, _, curr) => str + curr.toString,
@@ -323,7 +322,7 @@ class KyoTest extends Test:
             )
             assert(emptyTest.eval == "DEFAULT")
 
-            val arrayTest = shiftedWhile(Array(1, 2, 3, 4))(
+            val arrayTest = Kyo.shiftedWhile(Array(1, 2, 3, 4))(
                 prolog = "",
                 f = lessThanThree,
                 acc = (str, include, curr) => if include then str + curr.toString else str,
