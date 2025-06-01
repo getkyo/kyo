7bcd92909e30c044124ed38f045b1b75a48953a4
Jason Pickens <jasonpickensnz@gmail.com>
2025-06-01 21:32:26 +1200

    Remove unused fields

--- kyo-grpc/code-gen/shared/src/main/scala/kyo/grpc/compiler/Types.scala
+++ kyo-grpc/code-gen/shared/src/main/scala/kyo/grpc/compiler/Types.scala
@@ -2,17 +2,9 @@ package kyo.grpc.compiler
 
 object Types {
 
-    val unit = "_root_.scala.Unit"
-
-    def future(t: String) = s"_root_.scala.concurrent.Future[$t]"
-
-    def pending(t: String, s: String) = s"_root_.kyo.<[$t, $s]"
-
     def pendingGrpcResponse(t: String) = s"_root_.kyo.<[$t, $grpcResponse]"
 
     def pendingGrpcRequest(t: String) = s"_root_.kyo.<[$t, $grpcRequest]"
-
-    val frame = "_root_.kyo.Frame"
 
     def streamGrpcResponse(t: String) = s"_root_.kyo.Stream[$t, $grpcResponse]"
 
@@ -28,22 +20,10 @@ object Types {
 
     val service = "_root_.kyo.grpc.Service"
 
-    val javaServiceDescriptor = "_root_.com.google.protobuf.Descriptors.ServiceDescriptor"
-
     val callOptions = "_root_.io.grpc.CallOptions"
 
     val channel = "_root_.io.grpc.Channel"
 
     val serverServiceDefinition = "_root_.io.grpc.ServerServiceDefinition"
-
-    def streamObserver(v: String) = s"_root_.io.grpc.stub.StreamObserver[$v]"
-
-    val serverCalls = "_root_.io.grpc.stub.ServerCalls"
-
-    val serviceDescriptor = "_root_.scalapb.descriptors.ServiceDescriptor"
-
-    val abstractService = "_root_.scalapb.grpc.AbstractService"
-
-    def serviceCompanion(a: String) = s"_root_.scalapb.grpc.ServiceCompanion[$a]"
 
 }
--- kyo-grpc/code-gen/shared/src/main/scala/kyo/grpc/compiler/builders/Mod.scala
+++ kyo-grpc/code-gen/shared/src/main/scala/kyo/grpc/compiler/builders/Mod.scala
@@ -4,7 +4,6 @@ object Mod extends Choice {
     override type A = String
 
     val Case     = "case"
-    val Final    = "case"
     val Override = "override"
     val Private  = "private"
 }
