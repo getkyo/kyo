d59929b5b12c8d24a5bb8b54cd2e166cb7026cdf
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-06-09 18:22:11 +0200

    [prelude] move extension stream.unwrap to prelude

--- kyo-prelude/shared/src/test/scala/kyo/StreamTest.scala
+++ kyo-prelude/shared/src/test/scala/kyo/StreamTest.scala
@@ -1165,6 +1165,15 @@ class StreamTest extends Test:
 
             assert(someChoices == Chunk(1, 3))
         }
+
+        "extension" in {
+            val tick = Var.updateDiscard[Int](_ + 1)
+
+            val stream = tick.andThen(Stream.range(-1, 2)).unwrap
+
+            val n: Int = stream.foreach(_ => tick).handle(_.andThen(Var.get[Int]), Var.run(0)).eval
+            assert(n == 4)
+        }
     }
 
 end StreamTest
