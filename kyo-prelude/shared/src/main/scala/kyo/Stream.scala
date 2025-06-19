81745bc077dac501ebfbf11b9c4e967d864a7051
John Hungerford <hungerfordjustice@gmail.com>
2025-06-19 13:52:01 -0400

    Made sink contravariant with element type

--- kyo-prelude/shared/src/main/scala/kyo/Stream.scala
+++ kyo-prelude/shared/src/main/scala/kyo/Stream.scala
@@ -693,8 +693,8 @@ sealed abstract class Stream[+V, -S] extends Serializable:
       * @return
       *   An effect producing a value of sink's output type `A`
       */
-    def into[VV >: V, A, S2](sink: Sink[VV, A, S2])(using Tag[Poll[Chunk[VV]]], Tag[Emit[Chunk[VV]]], Frame): A < (S & S2) =
-        sink.drain(this)
+    def into[VV >: V, A, S2](sink: Sink[VV, A, S2])(using t1: Tag[Poll[Chunk[VV]]], t2: Tag[Emit[Chunk[VV]]], f: Frame): A < (S & S2) =
+        sink.drain[VV, S](this)
     end into
 
     /** Applies a transformation to this stream's underlying computation. This provides a convenient way to handle effects included in the
