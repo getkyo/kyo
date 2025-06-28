bf3340b35959f5e67d689a36dfc0d99a78edcfca
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2025-06-28 14:34:04 -0700

    Add tests for KyoApp signal (#1294)
    ### Problem
    - I missed adding tests for `KyoApp` signal interruption in #1260
    
    ### Solution
    - Add tests 🙂

---
+++ kyo-core/jvm/src/test/scala/kyo/KyoAppSignalTest.scala
@@ -0,0 +1,30 @@
+package kyo
+
+import kyo.*
+
+class KyoAppSignalTest extends Test:
+    object TestSignalApp extends KyoApp:
+        run {
+            for
+                _ <- Console.printLine("TestSignalApp started - waiting for signal")
+                _ <- Resource.ensure(ex => Console.printLine(s"TestSignalApp finished: $ex"))
+                _ <- Async.never
+            yield ()
+        }
+    end TestSignalApp
+
+    def testSignalInterruption(signal: String, exit: Int) =
+        s"SIG$signal interruption" taggedAs jvmOnly in run {
+            for
+                process   <- Process.jvm.spawn(TestSignalApp.getClass, List.empty)
+                _         <- Loop.whileTrue(process.isAlive.map(!_))(Async.sleep(20.millis))
+                pid       <- process.pid
+                _         <- Process.Command("kill", s"-$signal", pid.toString).waitFor
+                completed <- process.waitFor(3, java.util.concurrent.TimeUnit.SECONDS)
+                exitValue <- process.exitValue
+            yield assert(completed && exitValue == exit)
+        }
+
+    testSignalInterruption("TERM", 143)
+    testSignalInterruption("INT", 130)
+end KyoAppSignalTest
