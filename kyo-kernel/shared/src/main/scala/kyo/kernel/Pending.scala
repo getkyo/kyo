70ecb9037edab9caf4669072c469650f42d75b05
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-07-01 00:09:19 +0200

    avoid lifting modules (like Abort.type/Var.type, ...) (#1291)
    ~Plain solution about this problem. There is probably a better way.~
    
    ~@johnhungerford that's what I understood from: "Could we use implicit
    evidence to exclude certain objects from lifting?", you tell me!~
    
    
    ~- [x] add extends KyoModule to all the companion object of effects~
    
    
    Updating `WeakFlat` => `CanLift` with a macro to not lift modules in
    `kyo.` that are not a case object.
    
    ---------
    
    Co-authored-by: Adam Hearn <22334119+hearnadam@users.noreply.github.com>

--- kyo-kernel/shared/src/main/scala/kyo/kernel/Pending.scala
+++ kyo-kernel/shared/src/main/scala/kyo/kernel/Pending.scala
@@ -405,21 +405,25 @@ object `<`:
       *      proper effect composition.
       *   2. When the input is a regular value, it lifts it directly into the effect context through type casting.
       *
-      * The WeakFlat constraint avoids unexpected lifting when the pending effect set of computations don't match.
+      * The CanLift constraint avoids unexpected lifting when the pending effect set of computations don't match.
       *
       * @param v
       *   The value to lift into the effect context
       * @return
       *   A computation in the effect context
       */
-    implicit def lift[A: WeakFlat, S](v: A): A < S =
+    implicit def lift[A: CanLift, S](v: A): A < S =
         v match
             case kyo: Kyo[?, ?] => Nested(kyo)
             case _              => v.asInstanceOf[A < S]
 
-    implicit inline def liftUnit[S1, S2](inline v: Unit < S1): Unit < S2 = ${ liftUnitImpl[S1, S2]('v) }
+    implicit inline def liftAnyVal[A <: AnyVal, S](inline v: A): A < S = v.asInstanceOf[A < S]
 
-    private def liftUnitImpl[S1: Type, S2: Type](v: Expr[Unit < S1])(using quotes: Quotes): Expr[Unit < S2] =
+    implicit inline def liftUnit[S](inline v: Unit): Unit < S = v.asInstanceOf[Unit < S]
+
+    implicit inline def abortCastUnit[S1, S2](inline v: Unit < S1): Unit < S2 = ${ abortCastUnitImpl[S1, S2]('v) }
+
+    private def abortCastUnitImpl[S1: Type, S2: Type](v: Expr[Unit < S1])(using quotes: Quotes): Expr[Unit < S2] =
         import quotes.reflect.*
         val source = TypeRepr.of[S1].show
         report.errorAndAbort(
@@ -428,41 +432,41 @@ object `<`:
                |Consider removing or adjusting the type constraint on the left-hand side.
                |More info : https://github.com/getkyo/kyo/issues/903""".stripMargin
         )
-    end liftUnitImpl
+    end abortCastUnitImpl
 
     /** Converts a pure single-argument function to an effectful computation. */
     implicit inline def liftPureFunction1[A1, B](inline f: A1 => B)(
-        using inline flat: WeakFlat[B]
+        using inline flat: CanLift[B]
     ): A1 => B < Any =
         a1 => f(a1)
 
     /** Converts a pure two-argument function to an effectful computation. */
     implicit inline def liftPureFunction2[A1, A2, B](inline f: (A1, A2) => B)(
-        using inline flat: WeakFlat[B]
+        using inline flat: CanLift[B]
     ): (A1, A2) => B < Any =
         (a1, a2) => f(a1, a2)
 
     /** Converts a pure three-argument function to an effectful computation. */
     implicit inline def liftPureFunction3[A1, A2, A3, B](inline f: (A1, A2, A3) => B)(
-        using inline flat: WeakFlat[B]
+        using inline flat: CanLift[B]
     ): (A1, A2, A3) => B < Any =
         (a1, a2, a3) => f(a1, a2, a3)
 
     /** Converts a pure four-argument function to an effectful computation. */
     implicit inline def liftPureFunction4[A1, A2, A3, A4, B](inline f: (A1, A2, A3, A4) => B)(
-        using inline flat: WeakFlat[B]
+        using inline flat: CanLift[B]
     ): (A1, A2, A3, A4) => B < Any =
         (a1, a2, a3, a4) => f(a1, a2, a3, a4)
 
     /** Converts a pure five-argument function to an effectful computation. */
     implicit inline def liftPureFunction5[A1, A2, A3, A4, A5, B](inline f: (A1, A2, A3, A4, A5) => B)(
-        using inline flat: WeakFlat[B]
+        using inline flat: CanLift[B]
     ): (A1, A2, A3, A4, A5) => B < Any =
         (a1, a2, a3, a4, a5) => f(a1, a2, a3, a4, a5)
 
     /** Converts a pure six-argument function to an effectful computation. */
     implicit inline def liftPureFunction6[A1, A2, A3, A4, A5, A6, B](inline f: (A1, A2, A3, A4, A5, A6) => B)(
-        using inline flat: WeakFlat[B]
+        using inline flat: CanLift[B]
     ): (A1, A2, A3, A4, A5, A6) => B < Any =
         (a1, a2, a3, a4, a5, a6) => f(a1, a2, a3, a4, a5, a6)
 
