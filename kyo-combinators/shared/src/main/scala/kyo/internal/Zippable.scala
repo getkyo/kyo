4c49ed49e3d4ab1fabb92d896befb187a71776cc
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-07-01 12:06:48 +0200

    full names

--- kyo-combinators/shared/src/main/scala/kyo/internal/Zippable.scala
+++ kyo-combinators/shared/src/main/scala/kyo/internal/Zippable.scala
@@ -6,7 +6,7 @@ abstract class Zippable[-A, -B]:
     type Out
     def zip(left: A, right: B): Out
 
-object Zippable extends ZippableLowPrio0:
+object Zippable extends ZippableLowPriority0:
 
     given left[A]: (Zippable[A, Unit] { type Out = A })  = (left: A, _) => left
     given right[B]: (Zippable[Unit, B] { type Out = B }) = (_, right: B) => right
@@ -17,11 +17,11 @@ object Zippable extends ZippableLowPrio0:
     def zip[A, B](a: A, b: B)(using z: Zippable[A, B]): z.Out = z.zip(a, b)
 end Zippable
 
-trait ZippableLowPrio0 extends ZippableLowPrio1:
+trait ZippableLowPriority0 extends ZippableLowPriority1:
     given prepend[A, B <: Tuple]: (Zippable[A, B] { type Out = A *: B }) = (left: A, right: B) => left *: right
     given append[A <: Tuple, B]: (Zippable[A, B] { type Out = A :* B })  = (left: A, right: B) => left :* right
-end ZippableLowPrio0
+end ZippableLowPriority0
 
-trait ZippableLowPrio1:
+trait ZippableLowPriority1:
     given pair[A, B]: (Zippable[A, B] { type Out = (A, B) }) = (left: A, right: B) => (left, right)
-end ZippableLowPrio1
+end ZippableLowPriority1
