9b7b40f43e5b47ce5e55b6ce5a3fdf09e2d055ca
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-07-01 11:25:33 +0200

    add Zippable.prepend

--- kyo-combinators/shared/src/main/scala/kyo/internal/Zippable.scala
+++ kyo-combinators/shared/src/main/scala/kyo/internal/Zippable.scala
@@ -1,5 +1,7 @@
 package kyo.internal
 
+import scala.Tuple.*
+
 abstract class Zippable[-A, -B]:
     type Out
     def zip(left: A, right: B): Out
@@ -9,15 +11,17 @@ object Zippable extends ZippableLowPrio0:
     given left[A]: (Zippable[A, Unit] { type Out = A })  = (left: A, _) => left
     given right[B]: (Zippable[Unit, B] { type Out = B }) = (_, right: B) => right
 
-    given concat[A <: Tuple, B <: Tuple]: (Zippable[A, B] { type Out = Tuple.Concat[A, B] }) = (left: A, right: B) => left ++ right
+    given concat[A <: Tuple, B <: Tuple]: (Zippable[A, B] { type Out = A ++ B }) = (left: A, right: B) => left ++ right
 
-    def apply[A, B](using z: Zippable[A, B]): Zippable[A, B] = z
-
+    def apply[A, B](using z: Zippable[A, B]): Zippable[A, B]  = z
     def zip[A, B](a: A, b: B)(using z: Zippable[A, B]): z.Out = z.zip(a, b)
 end Zippable
 
 trait ZippableLowPrio0 extends ZippableLowPrio1:
-    given append[A <: Tuple, B]: (Zippable[A, B] { type Out = Tuple.Append[A, B] }) = (left: A, right: B) => left :* right
+    given prepend[A, B <: Tuple]: (Zippable[A, B] { type Out = A *: B }) = (left: A, right: B) => left *: right
+    given append[A <: Tuple, B]: (Zippable[A, B] { type Out = A :* B })  = (left: A, right: B) => left :* right
+end ZippableLowPrio0
 
 trait ZippableLowPrio1:
     given pair[A, B]: (Zippable[A, B] { type Out = (A, B) }) = (left: A, right: B) => (left, right)
+end ZippableLowPrio1
--- kyo-combinators/shared/src/test/scala/kyo/internal/ZippableTest.scala
+++ kyo-combinators/shared/src/test/scala/kyo/internal/ZippableTest.scala
@@ -5,18 +5,21 @@ import org.scalatest.compatible.Assertion
 
 class ZippableTest extends Test:
     "compile" - {
-        def check[A, B](using zip: Zippable[A, B])[Expected](using (zip.Out =:= Expected)): Assertion = succeed
+        def check[Left, Right](using zip: Zippable[Left, Right])[Expected](using (zip.Out =:= Expected)): Assertion = succeed
 
-        def on[A, B, C, D]: Assertion =
+        def on[A, B, C, D, E]: Assertion =
             check[(A, B), (C, D)][(A, B, C, D)]
-            check[Unit, A][A]
+            check[A, Unit][A]
             check[Unit, B][B]
             check[(A, B), Unit][(A, B)]
             check[(A, B), C][(A, B, C)]
+            check[A, (B, C)][(A, B, C)]
+            check[(A, B, C), D][(A, B, C, D)]
+            check[(A, B, C), (D, E)][(A, B, C, D, E)]
+
         end on
 
     }
-
     "zip" in {
         assert(Zippable.zip(1, 2) == (1, 2))
         assert(Zippable.zip(1, ()) == 1)
@@ -25,9 +28,6 @@ class ZippableTest extends Test:
         assert(Zippable.zip((1, 2), ()) == (1, 2))
         assert(Zippable.zip((), (3, 4)) == (3, 4))
         assert(Zippable.zip((1, 2), (3, 4)) == (1, 2, 3, 4))
-    }
-
-    "prepend" in {
-        assert(Zippable.zip(2, (3, 4)) == (2, (3, 4)))
+        assert(Zippable.zip(2, (3, 4)) == (2, 3, 4))
     }
 end ZippableTest
