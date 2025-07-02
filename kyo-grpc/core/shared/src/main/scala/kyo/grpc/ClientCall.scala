061b1d119567463f528d3ef0394f418463bbf861
Jason Pickens <jasonpickensnz@gmail.com>
2025-07-03 00:16:32 +1200

    Add docs to ClientCall

--- kyo-grpc/core/shared/src/main/scala/kyo/grpc/ClientCall.scala
+++ kyo-grpc/core/shared/src/main/scala/kyo/grpc/ClientCall.scala
@@ -6,8 +6,25 @@ import io.grpc.MethodDescriptor
 import kyo.*
 import scalapb.grpc.*
 
+/** Provides client-side gRPC call implementations for different RPC patterns.
+  *
+  * This object contains methods for executing unary, client streaming, server streaming,
+  * and bidirectional streaming gRPC calls using Kyo's effect system.
+  */
 object ClientCall:
 
+    /** Executes a unary gRPC call.
+      *
+      * A unary call sends a single request and receives a single response.
+      *
+      * @param channel the gRPC channel to use for the call
+      * @param method the method descriptor defining the RPC method
+      * @param options call options for configuring the request
+      * @param request the request message to send
+      * @tparam Request the type of the request message
+      * @tparam Response the type of the response message
+      * @return the response message pending [[GrpcRequest]]
+      */
     def unary[Request, Response](
         channel: Channel,
         method: MethodDescriptor[Request, Response],
@@ -18,6 +35,20 @@ object ClientCall:
         GrpcRequest.fromFuture(future)
     end unary
 
+    /** Executes a client streaming gRPC call.
+      *
+      * A client streaming call sends multiple requests via a stream and receives
+      * a single response. The client can send multiple messages over time, and
+      * the server responds with a single message after processing all requests.
+      *
+      * @param channel the gRPC channel to use for the call
+      * @param method the method descriptor defining the RPC method
+      * @param options call options for configuring the request
+      * @param requests a stream of request messages to send
+      * @tparam Request the type of the request messages
+      * @tparam Response the type of the response message
+      * @return the response message pending [[GrpcRequest]]
+      */
     def clientStreaming[Request: Tag, Response](
         channel: Channel,
         method: MethodDescriptor[Request, Response],
@@ -25,13 +56,27 @@ object ClientCall:
         requests: Stream[Request, GrpcRequest]
     )(using Frame, Tag[Emit[Chunk[Request]]]): Response < GrpcRequest =
         for
-            promise          <- Promise.init[GrpcResponse.Errors, Response]
+            promise          <- Promise.init[Grpc.Errors, Response]
             responseObserver <- Sync.Unsafe(UnaryResponseStreamObserver(promise))
             requestObserver = ClientCalls.asyncClientStreamingCall(channel, method, options, responseObserver)
             _        <- StreamNotifier.notifyObserver(requests, requestObserver)
             response <- promise.get
         yield response
 
+    /** Executes a server streaming gRPC call.
+      *
+      * A server streaming call sends a single request and receives multiple
+      * responses via a stream. The client sends one message, and the server
+      * responds with a stream of messages over time.
+      *
+      * @param channel the gRPC channel to use for the call
+      * @param method the method descriptor defining the RPC method
+      * @param options call options for configuring the request
+      * @param request the request message to send
+      * @tparam Request the type of the request message
+      * @tparam Response the type of the response messages
+      * @return a stream of response messages pending [[GrpcRequest]]
+      */
     def serverStreaming[Request, Response: Tag](
         channel: Channel,
         method: MethodDescriptor[Request, Response],
@@ -40,13 +85,27 @@ object ClientCall:
     )(using Frame, Tag[Emit[Chunk[Response]]]): Stream[Response, GrpcRequest] =
         val responses =
             for
-                responseChannel  <- StreamChannel.init[Response, GrpcResponse.Errors]
+                responseChannel  <- StreamChannel.init[Response, Grpc.Errors]
                 responseObserver <- Sync.Unsafe(ResponseStreamObserver(responseChannel))
                 _ = ClientCalls.asyncServerStreamingCall(channel, method, options, request, responseObserver)
             yield responseChannel.stream
         Stream.unwrap(responses)
     end serverStreaming
 
+    /** Executes a bidirectional streaming gRPC call.
+      *
+      * A bidirectional streaming call allows both client and server to send
+      * multiple messages via streams. Both sides can send messages independently
+      * and asynchronously, enabling full-duplex communication patterns.
+      *
+      * @param channel the gRPC channel to use for the call
+      * @param method the method descriptor defining the RPC method
+      * @param options call options for configuring the request
+      * @param requests a stream of request messages to send
+      * @tparam Request the type of the request messages
+      * @tparam Response the type of the response messages
+      * @return a stream of response messages pending [[GrpcRequest]]
+      */
     def bidiStreaming[Request: Tag, Response: Tag](
         channel: Channel,
         method: MethodDescriptor[Request, Response],
@@ -55,7 +114,7 @@ object ClientCall:
     )(using Frame, Tag[Emit[Chunk[Request]]], Tag[Emit[Chunk[Response]]]): Stream[Response, GrpcRequest] =
         val responses =
             for
-                responseChannel  <- StreamChannel.init[Response, GrpcResponse.Errors]
+                responseChannel  <- StreamChannel.init[Response, Grpc.Errors]
                 responseObserver <- Sync.Unsafe(ResponseStreamObserver(responseChannel))
                 requestObserver = ClientCalls.asyncBidiStreamingCall(channel, method, options, responseObserver)
                 _ <- StreamNotifier.notifyObserver(requests, requestObserver)
