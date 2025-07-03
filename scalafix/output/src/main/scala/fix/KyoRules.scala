9b34f74539b589aa0b985f835a875be3c8e818ce
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-07-04 00:05:51 +0200

    renames

--- scalafix/output/src/main/scala/fix/KyoRules.scala
+++
@@ -1,7 +0,0 @@
-package fix
-
-import kyo.*
-
-object KyoRules:
-    val prg = direct:
-        Console.printLine("hello").now
--- scalafix/rules/src/main/scala/kyo/rules/KyoUpdateToV1_0.scala
+++ scalafix/rules/src/main/scala/kyo/rules/KyoUpdateToV1_0.scala
@@ -1,9 +1,9 @@
-package fix
+package kyo.rules
 
 import scala.meta._
 import scalafix.v1._
 
-class KyoRules extends SemanticRule("KyoRules") {
+class KyoUpdateToV1_0 extends SemanticRule("KyoUpdateToV1_0") {
 
     override def fix(implicit doc: SemanticDocument): Patch = {
         println("Tree.syntax: " + doc.tree.syntax)
@@ -13,6 +13,8 @@ class KyoRules extends SemanticRule("KyoRules") {
         doc.tree.collect({
             case defer @ Term.Name("defer") if (SymbolMatcher.normalized("kyo.Direct$package.defer.").matches(defer.symbol)) =>
                 Patch.replaceTree(defer, "direct")
+
+            //TODO: IO => Sync
         }).asPatch
 
     }
