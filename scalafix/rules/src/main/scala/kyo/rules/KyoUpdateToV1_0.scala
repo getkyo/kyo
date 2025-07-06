fce63f5f1aacfeece44dca1408b245990b3fcfff
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-07-06 16:01:58 +0200

    simplify checks

--- scalafix/rules/src/main/scala/kyo/rules/KyoUpdateToV1_0.scala
+++ scalafix/rules/src/main/scala/kyo/rules/KyoUpdateToV1_0.scala
@@ -10,21 +10,25 @@ class KyoUpdateToV1_0 extends SemanticRule("KyoUpdateToV1_0") {
         println("Tree.structure: " + doc.tree.structure)
         println("Tree.structureLabeled: " + doc.tree.structureLabeled)
 
-
-
-
+        implicit class TreeOps(tree: Tree) {
+            def matches(symbol: String): Boolean = {
+                tree.symbol.normalized.value.matches("^" + symbol.flatMap(c =>
+                    if (c.isLetterOrDigit) c.toString else ".*"
+                ) + ".?$")
+            }
+        }
 
         doc.tree.collect({
-            case defer @ Term.Name("defer") if (SymbolMatcher.normalized("kyo.Direct$package.defer.").matches(defer.symbol)) =>
+            case defer @ Term.Name("defer") if defer matches "kyo.Direct.defer" =>
                 Patch.replaceTree(defer, "direct")
 
-            case ioApply @ Term.Select(Term.Name("IO"), Term.Name("apply")) if SymbolMatcher.normalized("kyo.IO$package.IO.apply.").matches(ioApply.symbol) =>
-                Patch.replaceTree(ioApply, "Sync.defer")
+            case apply @ Term.Select(Term.Name("IO"), Term.Name("apply")) if apply matches "kyo.IO.apply" =>
+                Patch.replaceTree(apply, "Sync.defer")
 
-            case Term.Apply.After_4_6_0(io @ Term.Name("IO"), _) if SymbolMatcher.normalized("kyo.IO$package.IO.").matches(io) =>
+            case Term.Apply.After_4_6_0(io @ Term.Name("IO"), _) if io matches "kyo.IO" =>
                 Patch.replaceTree(io, "Sync.defer")
 
-            case io @ Type.Name("IO") if SymbolMatcher.normalized("kyo/IO$package.IO#").matches(io) =>
+            case io @ Type.Name("IO") if io matches "kyo.IO" =>
                 Patch.replaceTree(io, "Sync")
         }).asPatch
 
