08b33a05978d9a46886052719167197fef462211
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-07-08 18:10:50 +0200

    [direct][stream] add stream support in direct (#1256)
    https://github.com/getkyo/kyo/issues/1221 add support for streams
    
    Forms:
    
    ```scala
    direct:
      stream.map(x => f(x).now)
    ```
    * could be valid if unwraped. -> compile error
    
    ---
    
    ```scala
    direct:
        stream.map(f)
    ```
    * should be supported without a compile error
    
    ---
    
    ```scala
    direct:
        stream.map(x => f(x).later)
    ```
    * should be supported without a compile error
    
    ---
    
    ```scala
    direct:
       stream.map(x => direct(f(x).now + 1))
    ```
    * should be supported without a compile error
    
    ---------
    
    Co-authored-by: Adam Hearn <22334119+hearnadam@users.noreply.github.com>

---
+++ kyo-direct/shared/src/test/scala/kyo/StreamTest.scala
@@ -0,0 +1,57 @@
+package kyo
+
+class StreamTest extends Test:
+
+    def check(stream: Stream[Int, Var[Int]] < Var[Int], expected: Chunk[Int]): Assertion < Any =
+        Var.run(0):
+            Stream.unwrap(stream).run.map: chunk =>
+                assert(chunk == expected)
+
+    "Stream" - {
+        "map" in run {
+            val stream = Stream.init(Seq(1, 2, 3))
+
+            def f(i: Int): Int < Var[Int] =
+                Var.update[Int](_ + i)
+
+            val magicStream = stream
+            typeCheckFailure(
+                """val newStream1: Stream[Int, Any] < Var[Int] = direct(magicStream.map(x => f(x).now))""".stripMargin
+            )("Calling `.now` inside a lazy structure breaks effect handling, and allow for escaping behavior.")
+
+            val newStream2: Stream[Int, Var[Int]] < Any = direct:
+                stream.map(x => f(x))
+
+            val newStream2_1: Stream[Int, Var[Int]] < Any = direct:
+                stream.map(f)
+
+            val newStream3 = direct:
+                stream.map(x =>
+                    direct:
+                        f(x).now + 1
+                )
+
+            val x = direct:
+                direct(1 + 1).now
+
+            check(newStream2, Chunk(1, 3, 6))
+
+        }
+
+        "filter" in run {
+            val stream = Stream.init(Seq(1, 2, 3))
+
+            def f(i: Int): Boolean < Var[Int] =
+                Var.update[Int](_ + i).map(_ % 2 == 0)
+
+            typeCheckFailure(
+                """val newStream1: Stream[Int, Any] < Var[Int] = direct(stream.filter(x => f(x).now))"""
+            )("Calling `.now` inside a lazy structure breaks effect handling, and allow for escaping behavior.")
+
+            val newStream2: Stream[Int, Var[Int]] < Any = direct:
+                stream.filter(x => f(x).later)
+
+            check(newStream2, Chunk(3))
+        }
+    }
+end StreamTest
