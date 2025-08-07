15b1d0609e07f51c49bdbbb2f6967176ab61db67
Iltotore <rafbodaha@gmail.com>
2025-08-07 12:40:17 +0200

    feat: Use Discard op instead of readAspect

--- kyo-parse/shared/src/main/scala/kyo/ParseState.scala
+++ kyo-parse/shared/src/main/scala/kyo/ParseState.scala
@@ -2,4 +2,4 @@ package kyo.parse
 
 import kyo.Chunk
 
-case class ParseState[In](input: ParseInput[In], failures: Chunk[ParseFailure])
+case class ParseState[In](input: ParseInput[In], failures: Chunk[ParseFailure], isDiscarded: In => Boolean = (_: In) => false)
