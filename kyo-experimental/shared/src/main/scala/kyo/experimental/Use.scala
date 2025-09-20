1bc6870a4e5752202a0bc9926282856cd236b8fd
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-09-21 00:04:17 +0200

    refactor `Use` and introduce `UseAsync` for improved modularity and clarity

--- kyo-experimental/shared/src/main/scala/kyo/experimental/Use.scala
+++ kyo-experimental/shared/src/main/scala/kyo/experimental/Use.scala
@@ -16,66 +16,47 @@ infix type &&[R1[-_], R2[-_]] = [S] =>> R1[S] & R2[S]
 
 object Use:
 
-    opaque type Async[+R[-_]] <: (Use[R] & kyo.Async) = ContextEffect[TypeMap[R[Any]]] & kyo.Async
-
-    private case class IsolateHolder(isolate: Isolate[?, kyo.Async, ?])
-    private object noOpIsolate extends Isolate[Any, Any, Any]:
-        type State        = Unit
-        type Transform[A] = A
-
-        override def capture[A, S](f: Unit => A < S)(using Frame): A < S        = f(())
-        override def isolate[A, S](state: Unit, v: A < S)(using Frame): <[A, S] = v
-        override def restore[A, S](v: <[A, S])(using Frame): A < S              = v
-    end noOpIsolate
-
-    private object IsolateFromEnv extends Isolate[Any, Any, Any]:
-        type State        = Any
-        type Transform[A] = Any
-
-        override def capture[A, S](f: IsolateFromEnv.State => A < S)(using Frame): A < S =
-            Env.use[IsolateHolder](_.isolate.capture(f)).asInstanceOf
-
-        override def isolate[A, S](state: IsolateFromEnv.State, v: A < S)(using Frame): <[Any, S] =
-            Env.use[IsolateHolder](_.isolate.isolate(state.asInstanceOf, v)).asInstanceOf
-
-        override def restore[A, S](v: <[Any, S])(using Frame): A < S =
-            Env.use[IsolateHolder](_.isolate.restore(v.asInstanceOf)).asInstanceOf
-    end IsolateFromEnv
-
-    inline given [R[-_]]: Isolate[Use.Async[R], kyo.Async, Use.Async[R]] = IsolateFromEnv.asInstanceOf
-
     private trait AnyT[-A]
 
     // We can erase R in the Tag because the implementation rely on a TypeMap that will do all the work
-    private def erasedTag[R[-_]] = Tag[Use[AnyT]].asInstanceOf[Tag[Use[R]]]
 
     // R[Use[R]] will force the use of R to add Use[R] to the pending set ... that has already Use[R]
     // it also solves cases where the effect is lifted (in a Stream, in a (A < S) < S2, ...
     // so we don't lose the type marker for Use[R] if it's not use directly
     // (if you would have used Context function for that, it's an use case for Caprese)
     def use[R[-_]](using frame: Frame)[A, S1](f: R[Use[R]] => A < S1)(using tag: Tag[R[Any]]): A < (Use[R] & S1) =
-        ContextEffect.suspendWith(erasedTag[R]): map =>
+        ContextEffect.suspendWith(internal.erasedTag[R]): map =>
             f(map.asInstanceOf[TypeMap[R[Any]]].get(using tag))
-
-    def useAsync[R[-_]](using frame: Frame)[A, S1](f: R[Use.Async[R]] => A < S1)(using tag: Tag[R[Any]]): A < (Use.Async[R] & S1) =
-        use(f)
 
     // run with R[S1] will provide R[?] to the A < (Use[R] & S2), and add S1 in the pending set
     def run[R[-_], S1](r: R[S1])[A, S2](a: A < (Use[R] & S2))(using tag: Tag[R[Any]], frame: Frame): A < (S1 & S2) =
         val env: TypeMap[R[Any]] = TypeMap(r.asInstanceOf[R[Any]])
-        ContextEffect.handle(erasedTag[R], env, _.union(env))(a)
+        ContextEffect.handle(internal.erasedTag[R], env, _.union(env))(a)
+
+    def get[R[-_]](using frame: Frame, tag: Tag[R[Any]]): R[Use[R]] < Use[R] = use[R](identity)
+
+    private[kyo] object internal:
+        def erasedTag[R[-_]]: Tag[Use[R]] = Tag[Use[AnyT]].asInstanceOf[Tag[Use[R]]]
+
+end Use
+
+opaque type UseAsync[+R[-_]] <: (Use[R] & kyo.Async) = ContextEffect[TypeMap[R[Any]]] & kyo.Async
+
+object UseAsync:
+
+    inline given [R[-_]]: Isolate[UseAsync[R], kyo.Async, UseAsync[R]] = null
+
+    def useAsync[R[-_]](using frame: Frame)[A, S1](f: R[UseAsync[R]] => A < S1)(using tag: Tag[R[Any]]): A < (UseAsync[R] & S1) =
+        Use.use(f)
 
     def runAsync[R[-_], S1](r: R[S1])(using
         isolate: Isolate[S1, kyo.Async, S1]
-    )[A, S2](a: A < (Use.Async[R] & S2))(using tap: Tag[R[Any]], frame: Frame): A < (S1 & S2 & kyo.Async) =
+    )[A, S2](a: A < (UseAsync[R] & S2))(using tap: Tag[R[Any]], frame: Frame): A < (S1 & S2 & kyo.Async) =
         val env: TypeMap[R[Any]] = TypeMap(r.asInstanceOf[R[Any]])
-        ContextEffect.handle(erasedTag[R], env, _.union(env))(
-            Env.run(IsolateHolder(Option(isolate).getOrElse(noOpIsolate)))(a)
-        )
+        ContextEffect.handle(Use.internal.erasedTag[R], env, _.union(env))(a)
     end runAsync
 
-    def get[R[-_]](using frame: Frame, tag: Tag[R[Any]]): R[Use[R]] < Use[R] = use[R](identity)
-end Use
+end UseAsync
 
 abstract class Service[+R[-_], -S] extends Serializable
 
