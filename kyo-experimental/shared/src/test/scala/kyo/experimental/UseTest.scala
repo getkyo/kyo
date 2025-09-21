0bc18e3d9f6a6b126c4f7d2a8c6479f97eece4e8
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-09-21 18:47:47 +0200

    add tests for `Use` and `UseAsync` covering service combination, subclass handling, and async behavior

--- kyo-experimental/shared/src/test/scala/kyo/experimental/UseTest.scala
+++ kyo-experimental/shared/src/test/scala/kyo/experimental/UseTest.scala
@@ -4,7 +4,6 @@ import kyo.*
 
 class UseTest extends kyo.Test:
 
-    // A minimal service demonstrating Use and UseAsync
     trait TestInt[-S]:
         def int: Int < S
 
@@ -25,6 +24,46 @@ class UseTest extends kyo.Test:
             Use.get[TestInt].map(_.int).map(v => assert(v == 42))
     }
 
+    "same instance returned by consecutive Use.get" in run {
+        Use.run(svc):
+            for
+                a <- Use.get[TestInt]
+                b <- Use.get[TestInt]
+            yield assert((a.asInstanceOf[AnyRef] eq b.asInstanceOf[AnyRef]))
+    }
+
+    "Use: passing subclass to a super service" in run {
+        trait Super[-S]:
+            def i: Int < S
+        class Sub extends Super[Sync]:
+            def i: Int < Sync = 99
+        val sub = new Sub
+        Use.run[Super, Sync](sub):
+            Use.use[Super](_.i).map(v => assert(v == 99))
+    }
+
+    // Two-service example inspired by EnvTest's pure services
+    trait Svc1[-S]:
+        def f(i: Int): Int < S
+
+    trait Svc2[-S]:
+        def g(i: Int): Int < S
+
+    "Use: combine two services with &&" in run {
+        val combined: Svc1[Sync] & Svc2[Sync] = new Svc1[Sync] with Svc2[Sync]:
+            def f(i: Int): Int < Sync = i * 2
+            def g(i: Int): Int < Sync = i * 3
+
+        val program: Int < (Use[Svc1 && Svc2]) =
+            Use.use: r =>
+                r.f(1).map: a =>
+                    r.g(2).map: b =>
+                        a + b
+
+        Use.run[Svc1 && Svc2, Sync](combined):
+            program.map(v => assert(v == 1 * 2 + 2 * 3))
+    }
+
     "UseAsync: race with a pure value" in run {
         val svc = new TestInt[Async & Sync]:
             def int: Int < (Async & Sync) = 1
@@ -42,5 +81,21 @@ class UseTest extends kyo.Test:
         UseAsync.run[TestInt, Async & Sync](svc) {
             getIntAsync_2.map(_ + 2).map(v => assert(v == 42))
         }
+    }
+
+    "UseAsync: service method can suspend with Async.sleep" in run {
+        val svc = new TestInt[Async & Sync]:
+            def int: Int < (Async & Sync) = Async.sleep(5.millis).andThen(41).map(_ + 1)
+
+        UseAsync.run(svc):
+            Async.zip(getIntAsync_1, getIntAsync_2).map: (a, b) =>
+                assert(a + b == 42 * 2)
+    }
+
+    "Should not compile" in {
+        val svc = new TestInt[Var[Int]]:
+            def int: Int < Var[Int] = Var.update(_ + 1)
+
+        typeCheckFailure("UseAsync.run(svc)(getIntAsync_2)")("S1 is a type variable with constraint")
     }
 end UseTest
