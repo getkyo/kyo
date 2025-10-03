d74d4d4921a12e2c5ce4b71ef28b535291430885
Jonathan Winandy <jonathan.winandy@gmail.com>
2025-10-03 22:09:06 +0200

    add `HKTMap` and tests for higher-kinded type-safe service composition using `kyo` effects

--- kyo-experimental/shared/src/main/scala/kyo/experimental/Use.scala
+++ kyo-experimental/shared/src/main/scala/kyo/experimental/Use.scala
@@ -4,7 +4,7 @@ import kyo.*
 import kyo.experimental.Service.internal
 import kyo.kernel.ContextEffect
 import scala.annotation.implicitNotFound
-
+import scala.collection.immutable.TreeSeqMap
 
 //todo : create a better Structure, ServiceMap[R, Any]
 
@@ -76,6 +76,170 @@ end UseAsync
 
 extension [A, R[-_], S](v: A < (Use[R] & S))
     def toUseAsync: A < (UseAsync[R] & S) = v
+
+opaque type HKTMap[+R[-_], -S] = TreeSeqMap[Tag[Any], Any]
+
+object HKTMap:
+
+    extension [R[-_], S](self: HKTMap[R, S])
+
+        private inline def fatal[T[-_]](using t: Tag[T[Any]]): Nothing =
+            throw new RuntimeException(s"fatal: kyo.experimental.HKTMap of contents [${self.show}] missing value of type: [${t.show}].")
+
+        /** Retrieves a value of higher-kinded type R2 from the HKTMap.
+          *
+          * @tparam R2
+          *   The higher-kinded type to retrieve (must be a supertype of R)
+          * @param t
+          *   An implicit Tag for type R2[Any]
+          * @param ev
+          *   Evidence that R[Any] is a subtype of R2[Any]
+          * @return
+          *   The value of type R2[S]
+          * @throws RuntimeException
+          *   if the value is not found
+          */
+        def get[R2[-_]](using t: Tag[R2[Any]], ev: R[Any] <:< R2[Any]): R2[S] =
+            def search: Any =
+                val it = self.iterator
+                while it.hasNext do
+                    val (tag, item) = it.next()
+                    if tag <:< t then
+                        return item
+                end while
+                fatal
+            end search
+            if isEmpty && t =:= Tag[Any] then ().asInstanceOf[R2[S]]
+            else self.getOrElse(t.erased, search).asInstanceOf[R2[S]]
+        end get
+
+        /** Adds a new HKT-value pair to the HKTMap.
+          *
+          * @param r2
+          *   The HKT instance to add
+          * @tparam R2
+          *   The higher-kinded type of the value to add
+          * @param t
+          *   An implicit Tag for type R2[Any]
+          * @return
+          *   A new HKTMap with the added HKT-value pair
+          */
+        inline def add[R2[-_]](r2: R2[S])(using inline t: Tag[R2[Any]]): HKTMap[R && R2, S] =
+            self.updated(t.erased, r2)
+
+        /** Combines this HKTMap with another HKTMap.
+          *
+          * @param that
+          *   The HKTMap to combine with
+          * @tparam R2
+          *   The higher-kinded type of values in the other HKTMap
+          * @tparam S2
+          *   The type parameter of the other HKTMap
+          * @return
+          *   A new HKTMap containing all HKT-value pairs from both HKTMaps
+          */
+        inline def union[R2[-_], S2](that: HKTMap[R2, S2]): HKTMap[R && R2, S & S2] =
+            self ++ that
+
+        /** Filters the HKTMap to only include HKT-value pairs where the HKT is a subtype of the given type.
+          *
+          * @tparam R2
+          *   The higher-kinded type to filter by (must be a supertype of R)
+          * @param t
+          *   An implicit Tag for type R2[Any]
+          * @return
+          *   A new HKTMap containing only the filtered HKT-value pairs
+          */
+        def prune[R2[-_] >: R](using t: Tag[R2[Any]]): HKTMap[R2, S] =
+            if t =:= Tag[Any] then self
+            else self.filter { case (tag, _) => tag <:< t }
+
+        /** Returns the number of HKT-value pairs in the HKTMap.
+          *
+          * @return
+          *   The size of the HKTMap
+          */
+        inline def size: Int = self.size
+
+        /** Checks if the HKTMap is empty.
+          *
+          * @return
+          *   true if the HKTMap is empty, false otherwise
+          */
+        inline def isEmpty: Boolean = self.isEmpty
+
+        /** Returns a string representation of the HKTMap.
+          *
+          * @return
+          *   A string describing the contents of the HKTMap
+          */
+        def show: String = self.map { case (tag, value) => s"${tag.show} -> $value" }.toList.sorted.mkString("HKTMap(", ", ", ")")
+
+        private[kyo] inline def <:<[T[-_]](tag: Tag[T[Any]]): Boolean =
+            self.keySet.exists(_ <:< tag)
+    end extension
+
+    /** An empty HKTMap. */
+    val empty: HKTMap[Const[Any], Any] = TreeSeqMap.empty(TreeSeqMap.OrderBy.Modification)
+
+    /** Creates an HKTMap with a single HKT-value pair.
+      *
+      * @param r
+      *   The HKT instance to add
+      * @tparam R
+      *   The higher-kinded type of the value
+      * @tparam S
+      *   The type parameter of the HKT
+      * @param tr
+      *   An implicit Tag for type R[Any]
+      * @return
+      *   A new HKTMap with the single HKT-value pair
+      */
+    def apply[R[-_], S](r: R[S])(using tr: Tag[R[Any]]): HKTMap[R, S] =
+        TreeSeqMap(tr.erased -> r).orderingBy(TreeSeqMap.OrderBy.Modification)
+
+    /** Creates an HKTMap with two HKT-value pairs.
+      *
+      * @param r1
+      *   The first HKT instance to add
+      * @param r2
+      *   The second HKT instance to add
+      * @tparam R1
+      *   The higher-kinded type of the first value
+      * @tparam R2
+      *   The higher-kinded type of the second value
+      * @tparam S
+      *   The type parameter of both HKTs
+      * @param tr1
+      *   An implicit Tag for type R1[Any]
+      * @param tr2
+      *   An implicit Tag for type R2[Any]
+      * @return
+      *   A new HKTMap with the two HKT-value pairs
+      */
+    def apply[R1[-_], R2[-_], S](r1: R1[S], r2: R2[S])(using tr1: Tag[R1[Any]], tr2: Tag[R2[Any]]): HKTMap[R1 && R2, S] =
+        TreeSeqMap(tr1.erased -> r1, tr2.erased -> r2).orderingBy(TreeSeqMap.OrderBy.Modification)
+
+    /** Creates an HKTMap with three HKT-value pairs.
+      */
+    def apply[R1[-_], R2[-_], R3[-_], S](r1: R1[S], r2: R2[S], r3: R3[S])(using
+        tr1: Tag[R1[Any]],
+        tr2: Tag[R2[Any]],
+        tr3: Tag[R3[Any]]
+    ): HKTMap[R1 && R2 && R3, S] =
+        TreeSeqMap(tr1.erased -> r1, tr2.erased -> r2, tr3.erased -> r3).orderingBy(TreeSeqMap.OrderBy.Modification)
+
+    /** Creates an HKTMap with four HKT-value pairs.
+      */
+    def apply[R1[-_], R2[-_], R3[-_], R4[-_], S](r1: R1[S], r2: R2[S], r3: R3[S], r4: R4[S])(using
+        tr1: Tag[R1[Any]],
+        tr2: Tag[R2[Any]],
+        tr3: Tag[R3[Any]],
+        tr4: Tag[R4[Any]]
+    ): HKTMap[R1 && R2 && R3 && R4, S] =
+        TreeSeqMap(tr1.erased -> r1, tr2.erased -> r2, tr3.erased -> r3, tr4.erased -> r4).orderingBy(TreeSeqMap.OrderBy.Modification)
+
+end HKTMap
 
 sealed abstract class Service[+R[-_], -S] extends Serializable:
     self =>
---
+++ kyo-experimental/shared/src/test/scala/kyo/experimental/HKTMapTest.scala
@@ -0,0 +1,325 @@
+package kyo.experimental
+
+import kyo.*
+
+class HKTMapTest extends Test:
+
+    trait Repository[-S]:
+        def save(item: String): Unit < S
+        def find: String < S
+
+    trait Cache[-S]:
+        def put(key: String, value: String): Unit < S
+        def get(key: String): Option[String] < S
+
+    trait Logger[-S]:
+        def log(level: String, message: String): Unit < S
+
+    // Concrete implementations that work with specific effect types
+    object InMemoryRepository extends Repository[Any]:
+        def save(item: String): Unit < Any = ()
+        def find: String < Any             = "found-item"
+
+    object SimpleCache extends Cache[Any]:
+        def put(key: String, value: String): Unit < Any = ()
+        def get(key: String): Option[String] < Any      = Some(s"cached-$key")
+
+    object ConsoleLogger extends Logger[Any]:
+        def log(level: String, message: String): Unit < Any =
+            println(s"[$level] $message")
+
+    "HKTMap with Kyo effects" - {
+        "empty" - {
+            "should create empty map" in {
+                val empty = HKTMap.empty
+                assert(empty.isEmpty)
+                assert(empty.size == 0)
+            }
+        }
+
+        "basic operations with effect-based services" - {
+            "should create map with single service" in run {
+                // Create HKTMap with Repository service
+                val map: HKTMap[Repository, Any] = HKTMap[Repository, Any](InMemoryRepository)
+
+                assert(map.size == 1)
+                val repo = map.get[Repository]
+
+                // Use the repository - effects are handled by the test framework
+                repo.save("test-item").map { _ =>
+                    repo.find.map { result =>
+                        assert(result == "found-item")
+                    }
+                }
+            }
+
+            "should work with multiple services" in run {
+                val repoMap: HKTMap[Repository, Any] = HKTMap[Repository, Any](InMemoryRepository)
+                val cacheMap: HKTMap[Cache, Any]     = HKTMap[Cache, Any](SimpleCache)
+
+                val combined = repoMap.union(cacheMap)
+                assert(combined.size == 2)
+
+                val repo  = combined.get[Repository]
+                val cache = combined.get[Cache]
+
+                // Test both services work
+                repo.find.map { repoResult =>
+                    cache.get("test-key").map { cacheResult =>
+                        assert(repoResult == "found-item")
+                        assert(cacheResult.contains("cached-test-key"))
+                    }
+                }
+            }
+        }
+
+        "union operations with effectful services" - {
+            "should combine compatible service maps" in run {
+                val repoMap: HKTMap[Repository, Any] = HKTMap[Repository, Any](InMemoryRepository)
+                val loggerMap: HKTMap[Logger, Any]   = HKTMap[Logger, Any](ConsoleLogger)
+
+                val combined = repoMap.union(loggerMap)
+                assert(combined.size == 2)
+
+                val repo   = combined.get[Repository]
+                val logger = combined.get[Logger]
+
+                // Test service composition
+                logger.log("INFO", "Starting operation").map { _ =>
+                    repo.save("important-data").map { _ =>
+                        logger.log("INFO", "Operation completed").map { _ =>
+                            succeed
+                        }
+                    }
+                }
+            }
+
+            "should handle service replacement" in run {
+                object AlternativeRepository extends Repository[Any]:
+                    def save(item: String): Unit < Any = ()
+                    def find: String < Any             = "alternative-result"
+
+                val map1: HKTMap[Repository, Any] = HKTMap[Repository, Any](InMemoryRepository)
+                val map2: HKTMap[Repository, Any] = HKTMap[Repository, Any](AlternativeRepository)
+
+                val combined = map1.union(map2)
+                assert(combined.size == 1)
+
+                val repo = combined.get[Repository]
+
+                repo.find.map { result =>
+                    assert(result == "alternative-result") // Second one wins
+                }
+            }
+        }
+
+        "constructor methods with services" - {
+            "should create map with multiple services directly" in run {
+                val map = HKTMap(InMemoryRepository, SimpleCache, ConsoleLogger)
+
+                assert(map.size == 3)
+
+                val repo   = map.get[Repository]
+                val cache  = map.get[Cache]
+                val logger = map.get[Logger]
+
+                // Test all services in a composition
+                logger.log("DEBUG", "Testing services").map { _ =>
+                    cache.put("test", "value").map { _ =>
+                        repo.save("test-data").map { _ =>
+                            cache.get("test").map { cached =>
+                                repo.find.map { found =>
+                                    assert(cached.contains("cached-test"))
+                                    assert(found == "found-item")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        "show and debugging" - {
+            "should provide readable string representation" in {
+                val map   = HKTMap(InMemoryRepository, SimpleCache)
+                val shown = map.show
+                assert(shown.contains("HKTMap"))
+                assert(shown.length > 10) // Should contain meaningful content
+            }
+        }
+
+        "type safety with effects" - {
+            "should maintain effect type information" in run {
+                val map: HKTMap[Repository, Any] = HKTMap[Repository, Any](InMemoryRepository)
+
+                // The retrieved repository should maintain its effect type
+                val repo: Repository[Any] = map.get[Repository]
+
+                // Should be able to use the repository with proper effects
+                repo.save("typed-data").map { _ =>
+                    repo.find.map { result =>
+                        assert(result == "found-item")
+                        assert(result.isInstanceOf[String])
+                    }
+                }
+            }
+        }
+
+        "advanced effect types" - {
+            "should work with Abort effects" in run {
+                // Define services that can fail with Abort
+                trait FailableRepository[-S]:
+                    def save(item: String): Unit < (S & Abort[String])
+                    def find(id: String): String < (S & Abort[String])
+
+                object ValidatingRepository extends FailableRepository[Any]:
+                    def save(item: String): Unit < Abort[String] =
+                        if item.nonEmpty then ()
+                        else Abort.fail("Item cannot be empty")
+
+                    def find(id: String): String < Abort[String] =
+                        if id == "valid" then s"found-$id"
+                        else Abort.fail("Item not found")
+                end ValidatingRepository
+
+                val map: HKTMap[FailableRepository, Abort[String]] =
+                    HKTMap[FailableRepository, Abort[String]](ValidatingRepository)
+
+                val repo = map.get[FailableRepository]
+
+                // Test successful case
+                Abort.run {
+                    repo.save("valid-item").map { _ =>
+                        repo.find("valid").map { result =>
+                            assert(result == "found-valid")
+                        }
+                    }
+                }.map { result =>
+                    assert(result.isSuccess)
+                }
+            }
+
+            "should work with Emit effects" in run {
+                // Define services that emit events
+                trait EventEmittingService[-S]:
+                    def processWithEvents(data: String): String < (S & Emit[String])
+
+                object AuditingService extends EventEmittingService[Any]:
+                    def processWithEvents(data: String): String < Emit[String] =
+                        Emit.value("Processing started").map { _ =>
+                            Emit.value(s"Processing data: $data").map { _ =>
+                                Emit.value("Processing completed").map { _ =>
+                                    s"processed-$data"
+                                }
+                            }
+                        }
+                end AuditingService
+
+                val map: HKTMap[EventEmittingService, Emit[String]] =
+                    HKTMap[EventEmittingService, Emit[String]](AuditingService)
+
+                val service = map.get[EventEmittingService]
+
+                // Test emit collection
+                Emit.run {
+                    service.processWithEvents("test-data")
+                }.map { (events, result) =>
+                    assert(result == "processed-test-data")
+                    assert(events.size == 3)
+                    assert(events.contains("Processing started"))
+                    assert(events.contains("Processing data: test-data"))
+                    assert(events.contains("Processing completed"))
+                }
+            }
+
+            "should work with combined effect types" in run {
+                // Service that uses multiple effects
+                trait ComplexService[-S]:
+                    def complexOperation(input: String): String < (S & Abort[String] & Emit[String])
+
+                object BusinessService extends ComplexService[Any]:
+                    def complexOperation(input: String): String < (Abort[String] & Emit[String]) =
+                        Emit.value(s"Starting operation with: $input").map { _ =>
+                            if input.isEmpty then Abort.fail("Input cannot be empty")
+                            else
+                                Emit.value("Validation passed").map { _ =>
+                                    Emit.value("Operation completed").map { _ =>
+                                        s"result-$input"
+                                    }
+                                }
+                        }
+                end BusinessService
+
+                val map: HKTMap[ComplexService, Abort[String] & Emit[String]] =
+                    HKTMap[ComplexService, Abort[String] & Emit[String]](BusinessService)
+
+                val service = map.get[ComplexService](using
+                    Tag[ComplexService[Any]],
+                    summon[ComplexService[Any] <:< ComplexService[Any]]
+                )
+
+                // Test successful case with both effects
+                Emit.run {
+                    Abort.run {
+                        service.complexOperation("valid-input")
+                    }
+                }.map { (events, abortResult) =>
+                    assert(abortResult.isSuccess)
+                    abortResult match
+                        case Result.Success(value) => assert(value == "result-valid-input")
+                        case Result.Failure(_)     => fail("Expected success")
+                    assert(events.size == 3)
+                    assert(events.contains("Starting operation with: valid-input"))
+                    assert(events.contains("Validation passed"))
+                    assert(events.contains("Operation completed"))
+                }
+            }
+
+            "should handle service composition with different effects" in run {
+                // Different services with different effect requirements
+                trait DatabaseService[-S]:
+                    def query(sql: String): List[String] < (S & Abort[String])
+
+                trait MetricsService[-S]:
+                    def recordMetric(name: String, value: Double): Unit < (S & Emit[String])
+
+                object PostgresService extends DatabaseService[Any]:
+                    def query(sql: String): List[String] < Abort[String] =
+                        if sql.contains("DROP") then Abort.fail("Dangerous query detected")
+                        else List(s"result-$sql")
+                end PostgresService
+
+                object PrometheusService extends MetricsService[Any]:
+                    def recordMetric(name: String, value: Double): Unit < Emit[String] =
+                        Emit.value(s"Metric recorded: $name = $value")
+
+                // Create separate maps for different effect types
+                val dbMap: HKTMap[DatabaseService, Abort[String]]    = HKTMap[DatabaseService, Abort[String]](PostgresService)
+                val metricsMap: HKTMap[MetricsService, Emit[String]] = HKTMap[MetricsService, Emit[String]](PrometheusService)
+
+                // Services can be used independently with their respective effects
+                val db      = dbMap.get[DatabaseService]
+                val metrics = metricsMap.get[MetricsService]
+
+                // Test database service
+                Abort.run {
+                    db.query("SELECT * FROM users")
+                }.map { dbResult =>
+                    assert(dbResult.isSuccess)
+                    dbResult match
+                        case Result.Success(value) => assert(value == List("result-SELECT * FROM users"))
+                        case Result.Failure(_)     => fail("Expected success")
+                    end match
+
+                    // Test metrics service
+                    Emit.run {
+                        metrics.recordMetric("query_count", 1.0)
+                    }.map { (events, _) =>
+                        assert(events.contains("Metric recorded: query_count = 1.0"))
+                    }
+                }
+            }
+        }
+    }
+
+end HKTMapTest
