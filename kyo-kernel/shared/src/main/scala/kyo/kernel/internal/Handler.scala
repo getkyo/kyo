69b9f4cb90a2d21aa1d132f9cce86ed1cecfdfa9
Flavio Brasil <fwbrasil@gmail.com>
2026-09-02 23:56:10 -0300

    [kernel] Handler: three open questions on moving the answering paths into Eval

--- kyo-kernel/shared/src/main/scala/kyo/kernel/internal/Handler.scala
+++ kyo-kernel/shared/src/main/scala/kyo/kernel/internal/Handler.scala
@@ -43,6 +43,7 @@ end Handler
     abstract class ContOpHandler[E <: Effect, A, B, S] extends ArrowHandler[Unit, E, A, B, S]:
         def run[X](operation: X < E, next: Arrow[X, A, E & S]): A < (E & S)
 
+        // TODO why isn't this in Eval?
         private[kyo] def answering[X](operation: X < E, next: Arrow[X, A, E & S], kyo: Pending[?, ?], stack: Stack): A < (E & S) =
             try run(operation, next)
             catch
@@ -54,6 +55,7 @@ end Handler
     abstract class LoopHandler[State, I[_], O[_], E <: ArrowEffect[I, O], A, B, S] extends ArrowHandler[State, E, A, B, S]:
         def run[X](state: State, input: I[X]): Outcome2[State, O[X] < (E & S), B < S] < S
 
+        // TODO why aren't these methods in Eval?
         private[kyo] def running[X](
             state: State,
             input: I[X],
@@ -133,6 +135,7 @@ end Handler
         private[kyo] def release(state: State, ex: Throwable): Unit = ()
     end ContextHandler
 
+    // TODO can we move this to Eval?
     private[kyo] inline def answersLoopState[State, I[_], O[_], E <: ArrowEffect[I, O], A, B, S, C](
         inline effectTag: Tag[E],
         inline handle: [X] => (State, I[X]) => Outcome2[State, O[X] < (E & S), B < (S)] < S,
